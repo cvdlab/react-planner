f671cef98a8d0e1b69219ca71a7bcd243513a61a
Christian Vadalà <chr.vadala@gmail.com>
2016-07-01 11:13:36 +0200

    show line thickness in 2d view

--- src/components/viewer2d/line.jsx
+++ src/components/viewer2d/line.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
-const STYLE_BASE = {stroke: "black", strokeWidth: "1px"};
-const STYLE_SELECTED = {stroke: "#FFC107", strokeWidth: "1px"};
-const STYLE_HOLE_BASE = {stroke: "black", strokeWidth: "3px"};
+const STYLE_BASE = {stroke: "#8E9BA2", strokeWidth: "1px", fill: "#e8e8e8"};
+const STYLE_SELECTED = {stroke: "#FFC107", strokeWidth: "1px", fill: "orange"};
+
+const STYLE_HOLE_BASE = {stroke: "#737373", strokeWidth: "3px", fill: "#737373"};
+const STYLE_HOLE_SELECTED = {stroke: "#737373", strokeWidth: "3px", fill: "orange"};
 
 const {pow, sqrt, asin, PI} = Math;
 
@@ -35,23 +37,26 @@ export default function Line({line, vertices, holes}) {
 
   let lenght = sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2));
   let angle = (-asin((y1 - y2) / lenght)) * 180 / PI;
-
+  let epsilon = line.thickness / 2;
 
   let holesComp = line.holes.map(holeID => {
     let hole = holes.get(holeID);
     let startAt = lenght * hole.offset - hole.width / 2;
+    let holePath = `M${0} ${ -epsilon}  L${hole.width} ${-epsilon}  L${hole.width} ${epsilon}  L${0} ${epsilon}  z`;
+    let holeStyle = hole.selected ? STYLE_HOLE_BASE : STYLE_HOLE_BASE;
     return (
       <g key={holeID} transform={`translate(${startAt}, 0)`}>
-        <line x1="0" y1="-10" x2="0" y2="10" style={STYLE_HOLE_BASE}/>
-        <line x1={hole.width} y1="-10" x2={hole.width} y2="10" style={STYLE_HOLE_BASE}/>
-        <line x1="0" y1="0" x2={hole.width} y2="0" style={STYLE_HOLE_BASE}/>
+        <path d={holePath} style={holeStyle}/>
+        <line x1={hole.width / 2} y1={- 10 - epsilon} x2={hole.width / 2} y2={10 + epsilon} style={holeStyle}/>
       </g>
     )
   });
 
+
+  let linePath = `M${0} ${ -epsilon}  L${lenght} ${-epsilon}  L${lenght} ${epsilon}  L${0} ${epsilon}  z`;
   return (
     <g transform={`translate(${x1}, ${y1}) rotate(${angle}, 0, 0)`}>
-      <line x1="0" y1="0" x2={lenght} y2="0" style={line.selected ? STYLE_SELECTED : STYLE_BASE}/>
+      <path d={linePath} style={line.selected ? STYLE_SELECTED : STYLE_BASE}/>
       {holesComp}
     </g>
   );
