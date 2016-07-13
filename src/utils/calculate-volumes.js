706ee4b1f3b0c6424e4288359c90c6be760c97d5
Christian Vadalà <chr.vadala@gmail.com>
2016-07-14 00:20:14 +0200

    fix calculate volumes

--- src/utils/calculate-volumes.js
+++ src/utils/calculate-volumes.js
@@ -5,46 +5,43 @@ import {ElementVolume} from '../models';
 
 export default function calculateVolumes(scene) {
 
+  let {pixelPerUnit} = scene;
+
   return new List().withMutations(list => {
     scene.layers.forEach(layer => {
-      layer.lines.forEach(({id, type, width, height, thickness}) =>
-        list.push(
-          new ElementVolume({
-            id: IDBroker.generate(),
-            elementID: id,
-            layerID: layer.id,
-            type: type,
-            prototype: "line",
-            volume: height * thickness,
-            composition: "cemento"
-          }))
+      let {vertices, lines} = layer;
+
+      layer.lines.forEach(({id, type, width, height, thickness, vertices: verticesIDs}) => {
+          let {x: x1, y:y1} = vertices.get(verticesIDs.get(0));
+          let {x: x2, y:y2} = vertices.get(verticesIDs.get(1));
+          let length = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+          list.push(
+            new ElementVolume({
+              id: IDBroker.generate(),
+              elementID: id,
+              layerID: layer.id,
+              type: type,
+              prototype: "lines",
+              volume: (height * thickness * length) / Math.pow(pixelPerUnit, 3),
+              composition: "cemento"
+            }))
+        }
       );
 
-      layer.holes.forEach(({id, type, width, height, thickness}) =>
+      layer.holes.forEach(({id, type, width, height, thickness, line: lineID}) => {
+
+        let line = lines.get(lineID);
         list.push(
           new ElementVolume({
             id: IDBroker.generate(),
             elementID: id,
             layerID: layer.id,
             type: type,
-            prototype: "hole",
-            volume: width * height * thickness,
+            prototype: "holes",
+            volume: width * height * line.thickness / Math.pow(pixelPerUnit, 3),
             composition: "vetro"
           }))
-      );
-
-      layer.areas.forEach(({id, type, width, height, thickness}) =>
-        list.push(
-          new ElementVolume({
-            id: IDBroker.generate(),
-            elementID: id,
-            layerID: layer.id,
-            type: type,
-            prototype: "area",
-            volume: width * height,
-            composition: "ceramica"
-          }))
-      )
+      });
 
     });
   });
