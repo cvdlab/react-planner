10124a74bcb3bd86fdb329886a6addd3f15cd6bb
Christian Vadalà <chr.vadala@gmail.com>
2016-07-19 11:29:19 +0200

    adds drawing structure

---
+++ src/reducers/drawing-reducer.js
@@ -0,0 +1,53 @@
+export default function (state, action) {
+
+}
+
+
+/** lines operations **/
+function beginDrawingLine(scene, x, y) {
+
+}
+
+function updateDrawingLine(scene, x, y) {
+
+}
+
+function endDrawingLine(scene, x, y) {
+
+}
+
+/** holes operations **/
+function beginDrawingHole(scene, x, y) {
+
+}
+
+function updateDrawingHole(scene, x, y) {
+
+}
+
+function endDrawingHole(scene, x, y) {
+
+}
+
+/** lines features **/
+function addLine(scene, vertex1, vertex2) {
+
+}
+
+function removeLine(scene, lineID) {
+
+}
+
+function splitLine(scene, lineID, x, y) {
+
+}
+
+
+/** vertices features **/
+function addVertex(scene, x, y) {
+
+}
+
+function removeVertex(scene, vertexID) {
+
+}
---
+++ src/utils/drawing-helpers.js
@@ -0,0 +1,33 @@
+export function nearestSnapPoint(drawingHelpers, xp, yp, minRadius) {
+
+  return {x: 0, y: 0, distance: 0};
+}
+
+export function addPointHelper(drawingHelpers, x, y, radius, priority) {
+
+  return [drawingHelpers, {}];
+}
+
+export function addLineHelper(drawingHelpers, m, q, radius, priority) {
+
+  return [drawingHelpers, {}];
+}
+
+export function addLineSegmentHelper(drawingHelpers, x1, y1, x2, y2, radius, priority) {
+
+  return [drawingHelpers, {}];
+}
+
+export function addHorizontalLineHelper(drawingHelpers, y, radius, priority) {
+
+  return [drawingHelpers, {}];
+}
+
+export function addVerticalLineHelper(drawingHelpers, x, radius, priority) {
+
+  return [drawingHelpers, {}];
+}
+
+
+
+
