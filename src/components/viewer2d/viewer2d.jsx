257cfb9435ae4e3060b8d1554b17645485a1f70d
Christian Vadalà <chr.vadala@gmail.com>
2016-07-19 16:03:47 +0200

    adds viewer2d interactions

--- src/components/viewer2d/viewer2d.jsx
+++ src/components/viewer2d/viewer2d.jsx
@@ -1,35 +1,88 @@
 "use strict";
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, {PropTypes} from 'react';
 
 import {Viewer, ViewerHelper, TOOL_NONE, TOOL_PAN, TOOL_ZOOM_IN, TOOL_ZOOM_OUT} from 'react-svg-pan-zoom';
-import {MODE_2D_PAN, MODE_2D_ZOOM_IN, MODE_2D_ZOOM_OUT} from '../../constants';
+import {
+  MODE_IDLE,
+  MODE_2D_PAN,
+  MODE_2D_ZOOM_IN,
+  MODE_2D_ZOOM_OUT,
+  MODE_WAITING_DRAWING_LINE,
+  MODE_DRAWING_LINE,
+  MODE_WAITING_DRAWING_HOLE,
+  MODE_DRAWING_HOLE
+} from '../../constants';
 import Scene from './scene.jsx';
 
-const TOOLS_MAP = {
-  MODE_2D_PAN: TOOL_PAN,
-  MODE_2D_ZOOM_IN: TOOL_ZOOM_IN,
-  MODE_2D_ZOOM_OUT: TOOL_ZOOM_OUT
-};
 
-
-export default function Viewer2D({scene, width, height, viewer2D, mode}, {editingActions, viewer2DActions}) {
-
-  if (!viewer2D.has('mode')) {
-    viewer2D = ViewerHelper.getDefaultValue();
-    viewer2D = ViewerHelper.fitSVGToViewer(viewer2D, scene.width, scene.height, width, height);
-  } else {
-    viewer2D = viewer2D.toJS();
+function mode2Tool(mode) {
+  switch (mode) {
+    case MODE_2D_PAN:
+      return TOOL_PAN;
+    case MODE_2D_ZOOM_IN:
+      return TOOL_ZOOM_IN;
+    case MODE_2D_ZOOM_OUT:
+      return TOOL_ZOOM_OUT;
+    default:
+      return TOOL_NONE;
   }
+}
 
 
-  let onClick = event => editingActions.unselectAll();
+export default function Viewer2D({scene, width, height, viewer2D, mode}, {editingActions, viewer2DActions, drawingActions}) {
+
+  viewer2D = viewer2D.isEmpty() ? ViewerHelper.getDefaultValue() : viewer2D.toJS();
+
+  let mapCursorPosition = ({x, y}) => {
+    return {x, y: -y + scene.height}
+  };
+
+  let onClick = event => {
+    let {x, y} = mapCursorPosition(event);
+
+    switch (mode) {
+      case MODE_IDLE:
+        editingActions.unselectAll();
+        break;
+
+      case MODE_WAITING_DRAWING_LINE:
+        drawingActions.beginDrawingLine(x, y);
+        break;
+
+      case MODE_DRAWING_LINE:
+        drawingActions.endDrawingLine(x, y);
+        break;
+
+      case MODE_WAITING_DRAWING_HOLE:
+        drawingActions.beginDrawingHole(x, y);
+        break;
+
+      case MODE_DRAWING_HOLE:
+        drawingActions.endDrawingHole(x, y);
+        break;
+    }
+  };
+
+  let onMouseMove = event => {
+    let {x, y} = mapCursorPosition(event);
+
+    switch (mode) {
+      case MODE_DRAWING_LINE:
+        drawingActions.updateDrawingLine(x, y);
+        break;
+
+      case MODE_DRAWING_HOLE:
+        drawingActions.updateDrawingHole(x, y);
+        break;
+    }
+  };
+
   let onChange = event => viewer2DActions.updateCameraView(event.value);
-  let viewerTool = TOOLS_MAP.hasOwnProperty(mode) ? TOOLS_MAP[mode] : TOOL_NONE;
 
   return (
-    <Viewer value={viewer2D} onChange={onChange} onClick={onClick} tool={viewerTool} width={width} height={height}>
+    <Viewer value={viewer2D} tool={mode2Tool(mode)} width={width} height={height}
+            onMouseMove={onMouseMove} onChange={onChange} onClick={onClick}>
       <svg width={scene.width} height={scene.height} style={{cursor: "crosshair"}}>
         <Scene scene={scene}/>
       </svg>
@@ -39,14 +92,15 @@ export default function Viewer2D({scene, width, height, viewer2D, mode}, {editin
 
 
 Viewer2D.propTypes = {
-  mode: React.PropTypes.string.isRequired,
-  scene: React.PropTypes.object.isRequired,
-  width: React.PropTypes.number.isRequired,
-  height: React.PropTypes.number.isRequired,
-  viewer2D: React.PropTypes.object.isRequired
+  mode: PropTypes.string.isRequired,
+  scene: PropTypes.object.isRequired,
+  width: PropTypes.number.isRequired,
+  height: PropTypes.number.isRequired,
+  viewer2D: PropTypes.object.isRequired
 };
 
 Viewer2D.contextTypes = {
-  viewer2DActions: React.PropTypes.object.isRequired,
-  editingActions: React.PropTypes.object.isRequired
+  viewer2DActions: PropTypes.object.isRequired,
+  editingActions: PropTypes.object.isRequired,
+  drawingActions: PropTypes.object.isRequired
 };
