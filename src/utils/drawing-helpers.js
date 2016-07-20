babb2013344124627f1e33e2aff9a3d032188634
Christian Vadalà <chr.vadala@gmail.com>
2016-07-20 17:24:17 +0200

    adds snapPoints

--- src/utils/drawing-helpers.js
+++ src/utils/drawing-helpers.js
@@ -1,11 +1,34 @@
-export function nearestSnapPoint(drawingHelpers, xp, yp, minRadius) {
+import {List, Record} from 'immutable';
+import {distanceFromTwoPoints} from './geometry';
 
-  return {x: 0, y: 0, distance: 0};
+class PointHelper extends Record({x: -1, y: -1, radius: 1, priority: 1, related: new List()}) {
+  nearestSnapPoint(x, y) {
+    return {
+      x: this.x,
+      y: this.y,
+      distance: distanceFromTwoPoints(this.x, this.y, x, y)
+    };
+  }
 }
 
-export function addPointHelper(drawingHelpers, x, y, radius, priority) {
+export function nearestSnapPoint(drawingHelpers, x, y, minDistance) {
 
-  return [drawingHelpers, {}];
+  let mapper = pointElement => pointElement.nearestSnapPoint(x, y);
+  let comparator = ({distance: distance1}, {distance: distance2}) => distance1 - distance2;
+
+  if (drawingHelpers.isEmpty()) return undefined;
+
+  let nearestSnapPoint = drawingHelpers.minBy(mapper, comparator);
+
+  let result = nearestSnapPoint.nearestSnapPoint(x, y);
+  if (result.distance > minDistance) return undefined;
+
+  return result;
+}
+
+export function addPointHelper(drawingHelpers, x, y, radius, priority, related) {
+  related = new List([related]);
+  return drawingHelpers.push(new PointHelper({x, y, radius, priority, related}));
 }
 
 export function addLineHelper(drawingHelpers, m, q, radius, priority) {
@@ -29,5 +52,12 @@ export function addVerticalLineHelper(drawingHelpers, x, radius, priority) {
 }
 
 
+//DEBUG
+let drawingHelpers = new List();
+drawingHelpers = addPointHelper(drawingHelpers, 100, 100, 20, 1, 'abc');
+drawingHelpers = addPointHelper(drawingHelpers, 200, 200, 20, 1, 'cde');
+drawingHelpers = addPointHelper(drawingHelpers, 150, 150, 20, 1, 'fgh');
 
+console.log(drawingHelpers.toJS());
 
+console.log(1, nearestSnapPoint(drawingHelpers, 140, 150, 99999));
---
+++ src/utils/geometry.js
@@ -0,0 +1,3 @@
+export function distanceFromTwoPoints(x0, y0, x1, y1) {
+  return Math.sqrt(Math.pow((x1 - x0), 2) + Math.pow((y1 - y0), 2));
+}
