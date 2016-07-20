72705ddb99c7228bb9b13239ccb76685eb32cca6
Christian Vadalà <chr.vadala@gmail.com>
2016-07-20 12:43:51 +0200

    adds lines features

--- src/reducers/drawing-reducer.js
+++ src/reducers/drawing-reducer.js
@@ -93,16 +93,53 @@ function endDrawingHole(scene, layerID, x, y) {
 }
 
 /** lines features **/
-function addLine(scene, layerID, vertex1, vertex2) {
-  return scene;
+function addLine(layer, type, x1, y1, x2, y2) {
+  let line;
+
+  layer = layer.withMutations(layer => {
+    let lineID = IDBroker.acquireID();
+
+    let v1, v2;
+    ({layer, vertex: v1} = addVertex(layer, x1, y1, 'lines', lineID));
+    ({layer, vertex: v2} = addVertex(layer, x2, y2, 'lines', lineID));
+
+    line = new Line({
+      id: lineID,
+      vertices: new List([v1.id, v2.id]),
+      type
+    });
+
+    layer.setIn(['lines', lineID], line);
+  });
+
+  return {layer, line};
 }
 
-function removeLine(scene, layerID, lineID) {
-  return scene;
+function removeLine(layer, lineID) {
+  let line = layer.getIn(['lines', lineID]);
+
+  layer = layer.withMutations(layer => {
+    layer.deleteIn(['lines', line.id]);
+    line.vertices.forEach(vertexID => removeVertex(layer, vertexID, 'lines', line.id));
+  });
+
+  return {layer, line};
 }
 
-function splitLine(scene, layerID, lineID, x, y) {
-  return scene;
+function splitLine(layer, lineID, x, y) {
+  let line0, line1;
+
+  layer = layer.withMutations(layer => {
+    let line = layer.getIn(['lines', lineID]);
+    let {x: x1, y: y1} = layer.vertices.get(line.vertices.get(0));
+    let {x: x2, y: y2} = layer.vertices.get(line.vertices.get(1));
+
+    removeLine(layer, lineID);
+    ({line: line0} = addLine(layer, line.type, x1, y1, x, y));
+    ({line: line1} = addLine(layer, line.type, x2, y2, x, y));
+  });
+
+  return {layer, lines: new List([line0, line1])};
 }
 
 
@@ -118,7 +155,8 @@ function addVertex(layer, x, y, relatedPrototype, relatedID) {
       [relatedPrototype]: new List([relatedID])
     });
   }
-  return layer.setIn(['vertices', vertex.id], vertex);
+  layer = layer.setIn(['vertices', vertex.id], vertex);
+  return {layer, vertex};
 }
 
 function removeVertex(layer, vertexID, relatedPrototype, relatedID) {
@@ -126,10 +164,11 @@ function removeVertex(layer, vertexID, relatedPrototype, relatedID) {
   vertex = vertex.update(relatedPrototype, related => related.filter(ID => relatedID !== ID));
 
   if (vertex.areas.size + vertex.lines.size === 0) {
-    return layer.deleteIn(['vertices', vertex.id]);
+    layer = layer.deleteIn(['vertices', vertex.id]);
   } else {
-    return layer.setIn(['vertices', vertex.id], vertex);
+    layer = layer.setIn(['vertices', vertex.id], vertex);
   }
+  return {layer, vertex};
 }
 
 
