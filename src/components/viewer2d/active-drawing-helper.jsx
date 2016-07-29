06589d5cd7e0ffeaa3b78b6513262a155244a1a1
Christian Vadalà <chr.vadala@gmail.com>
2016-07-29 21:16:37 +0200

    enhances active drawing helper point

--- src/components/viewer2d/active-drawing-helper.jsx
+++ src/components/viewer2d/active-drawing-helper.jsx
@@ -3,9 +3,13 @@ import React, {PropTypes} from 'react';
 export default function ActiveDrawingHelper({helper}) {
   console.log(helper.type, helper.x, helper.y);
 
-  switch(helper.type){
+  switch (helper.type) {
     case 'point':
-      return <circle cx={helper.x} cy={helper.y} r={helper.radius} fill="#000"/>
+      return (
+        <g transform={`translate(${helper.x} ${helper.y})`}>
+          <line x1="0" y1="-70" x2="0" y2="70" stroke="#ff0000"/>
+          <line x1="-70" y1="0" x2="70" y2="0" stroke="#ff0000"/>
+        </g>);
 
     default:
       return null;
