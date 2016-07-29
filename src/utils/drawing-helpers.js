bd19a7c6c25b42d02462158a5220b32a16861d2a
Christian Vadalà <chr.vadala@gmail.com>
2016-07-29 23:04:24 +0200

    enhances line helper with intersections check

--- src/utils/drawing-helpers.js
+++ src/utils/drawing-helpers.js
@@ -1,7 +1,7 @@
 import {List, Record} from 'immutable';
 import * as Geometry from './geometry';
 
-class PointHelper extends Record({type:"point", x: -1, y: -1, radius: 1, priority: 1, related: new List()}) {
+class PointHelper extends Record({type: "point", x: -1, y: -1, radius: 1, priority: 1, related: new List()}) {
   nearestPoint(x, y) {
     return {
       x: this.x,
@@ -11,7 +11,7 @@ class PointHelper extends Record({type:"point", x: -1, y: -1, radius: 1, priorit
   }
 }
 
-class LineHelper extends Record({type:"line", a: -1, b: -1, c: -1, radius: 1, priority: 1, related: new List()}) {
+class LineHelper extends Record({type: "line", a: -1, b: -1, c: -1, radius: 1, priority: 1, related: new List()}) {
   nearestPoint(x, y) {
     return {
       ...Geometry.closestPointFromLine(this.a, this.b, this.c, x, y),
@@ -29,9 +29,9 @@ export function nearestDrawingHelper(drawingHelpers, x, y) {
     })
     .filter(({helper: {radius}, point: {distance}}) => distance < radius)
     .min(({helper: helper1, point: point1}, {helper: helper2, point: point2}) => {
-      if(helper1.priority === helper2.priority) {
+      if (helper1.priority === helper2.priority) {
         if (point1.distance < point2.distance) return -1; else return 1;
-      }else{
+      } else {
         if (helper1.priority > helper2.priority) return -1; else return 1;
       }
     });
@@ -46,7 +46,18 @@ export function addPointHelper(drawingHelpers, x, y, radius, priority, related) 
 
 export function addLineHelper(drawingHelpers, a, b, c, radius, priority, related) {
   related = new List([related]);
-  return drawingHelpers.push(new LineHelper({a, b, c, radius, priority, related}));
+
+  return drawingHelpers.withMutations(drawingHelpers => {
+
+    let intersections = drawingHelpers
+      .valueSeq()
+      .filter(helper => helper.type === 'line')
+      .map(helper => Geometry.intersectionFromTwoLines(helper.a, helper.b, helper.c, a, b, c))
+      .filter(intersection => intersection !== undefined)
+      .forEach(({x, y}) => addPointHelper(drawingHelpers, x, y, 20, 40));
+
+    drawingHelpers.push(new LineHelper({a, b, c, radius, priority, related}));
+  })
 }
 
 export function addLineSegmentHelper(drawingHelpers, x1, y1, x2, y2, radius, priority) {
