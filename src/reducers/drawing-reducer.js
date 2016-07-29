73450f09c53d0dffd402d4fc7db12f22bf9c739f
Christian Vadalà <chr.vadala@gmail.com>
2016-07-29 13:50:17 +0200

    refactor change drawing reducer

--- src/reducers/drawing-reducer.js
+++ src/reducers/drawing-reducer.js
@@ -21,37 +21,25 @@ export default function (state, action) {
       return state.set('mode', MODE_WAITING_DRAWING_LINE);
 
     case BEGIN_DRAWING_LINE:
-      return state.merge({
-        mode: MODE_DRAWING_LINE,
-        scene: beginDrawingLine(state.scene, action.layerID, action.x, action.y)
-      });
+      return beginDrawingLine(state, action.layerID, action.x, action.y);
 
     case UPDATE_DRAWING_LINE:
-      return state.set('scene', updateDrawingLine(state.scene, action.layerID, action.x, action.y));
+      return updateDrawingLine(state, action.layerID, action.x, action.y);
 
     case END_DRAWING_LINE:
-      return state.merge({
-        mode: MODE_WAITING_DRAWING_LINE,
-        scene: endDrawingLine(state.scene, action.layerID, action.x, action.y)
-      });
+      return endDrawingLine(state, action.layerID, action.x, action.y);
 
     case SELECT_TOOL_DRAWING_HOLE:
       return state.set('mode', MODE_WAITING_DRAWING_HOLE);
 
     case BEGIN_DRAWING_HOLE:
-      return state.merge({
-        mode: MODE_DRAWING_HOLE,
-        scene: updateDrawingHole(state.scene, action.layerID, action.x, action.y)
-      });
+      return updateDrawingHole(state, action.layerID, action.x, action.y);
 
     case UPDATE_DRAWING_HOLE:
-      return state.set('scene', updateDrawingHole(state.scene, action.layerID, action.x, action.y));
+      return updateDrawingHole(state, action.layerID, action.x, action.y);
 
     case END_DRAWING_HOLE:
-      return state.merge({
-        mode: MODE_WAITING_DRAWING_HOLE,
-        scene: endDrawingHole(state.scene, action.layerID, action.x, action.y)
-      });
+      return endDrawingHole(state, action.layerID, action.x, action.y);
 
     default:
       return state;
@@ -60,40 +48,46 @@ export default function (state, action) {
 
 
 /** lines operations **/
-function beginDrawingLine(scene, layerID, x, y) {
-  return scene.updateIn(['layers', layerID], layer => layer.withMutations(layer => {
+function beginDrawingLine(state, layerID, x, y) {
+  let scene = state.scene.updateIn(['layers', layerID], layer => layer.withMutations(layer => {
     let {line} = addLine(layer, 'wall-generic', x, y, x, y);
     select(layer, 'lines', line.id);
   }));
+
+  return state.merge({mode: MODE_DRAWING_LINE, scene});
 }
 
-function updateDrawingLine(scene, layerID, x, y) {
-  return scene.updateIn(['layers', layerID], layer => {
+function updateDrawingLine(state, layerID, x, y) {
+  let scene = state.scene.updateIn(['layers', layerID], layer => {
     let lineID = layer.getIn(['selected', 'lines']).first();
     ({layer} = replaceLineVertex(layer, lineID, 1, x, y));
     return layer;
   });
+
+  return state.merge({scene});
 }
 
-function endDrawingLine(scene, layerID, x, y) {
-  return scene.updateIn(['layers', layerID], layer => {
+function endDrawingLine(state, layerID, x, y) {
+  let scene = state.scene.updateIn(['layers', layerID], layer => {
     let lineID = layer.getIn(['selected', 'lines']).first();
     let line = layer.getIn(['lines', lineID]);
     ({layer} = replaceLineVertex(layer, lineID, 1, x, y));
     layer = unselect(layer, 'lines', lineID);
     return layer;
   });
+
+  return state.merge({mode: MODE_WAITING_DRAWING_LINE, scene});
 }
 
 /** holes operations **/
-function beginDrawingHole(scene, layerID, x, y) {
-  return scene;
+function beginDrawingHole(state, layerID, x, y) {
+  return state.merge({mode: MODE_DRAWING_HOLE});
 }
 
-function updateDrawingHole(scene, layerID, x, y) {
-  return scene;
+function updateDrawingHole(state, layerID, x, y) {
+  return state;
 }
 
-function endDrawingHole(scene, layerID, x, y) {
-  return scene;
+function endDrawingHole(state, layerID, x, y) {
+  return state.merge({mode: MODE_WAITING_DRAWING_HOLE});
 }
