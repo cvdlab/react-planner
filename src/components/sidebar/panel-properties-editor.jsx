c20e8fb6d2482558a92ef7391b0640d4e5200670
Christian Vadalà <chr.vadala@gmail.com>
2016-08-02 13:12:00 +0200

    hide panel properties if in not compatible mode

--- src/components/sidebar/panel-properties-editor.jsx
+++ src/components/sidebar/panel-properties-editor.jsx
@@ -2,8 +2,11 @@ import React, {PropTypes} from 'react';
 import Panel from './panel.jsx';
 import PropertiesEditor from '../properties-editor/properties-editor.jsx';
 import {Seq} from 'immutable';
+import {MODE_IDLE, MODE_3D_VIEW, MODE_3D_FIRST_PERSON} from '../../constants';
 
-export default function PanelPropertiesEditor({scene}) {
+export default function PanelPropertiesEditor({scene, mode}) {
+
+  if (![MODE_IDLE, MODE_3D_VIEW, MODE_3D_FIRST_PERSON].includes(mode)) return null;
 
   let componentRenderer = (element, layer) =>
     <Panel key={element.id} name={`Properties: [${element.type}] ${element.id}`}>
@@ -23,5 +26,6 @@ export default function PanelPropertiesEditor({scene}) {
 }
 
 PanelPropertiesEditor.propTypes = {
-  scene: PropTypes.object.isRequired
+  scene: PropTypes.object.isRequired,
+  mode: PropTypes.string.isRequired
 };
--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -6,7 +6,7 @@ export default function Sidebar({width, height, state}) {
   return (
     <aside style={{backgroundColor: "#28292D", display:"block", width, height}}>
       <PanelMousePosition />
-      <PanelPropertiesEditor scene={state.scene}/>
+      <PanelPropertiesEditor scene={state.scene} mode={state.mode}/>
     </aside>
   );
 }
