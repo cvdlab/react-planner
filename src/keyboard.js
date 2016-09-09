111e876d84f2ace973d808ce91149c48c4518bb1
Christian Vadalà <chr.vadala@gmail.com>
2016-09-09 15:30:01 +0200

    adds delete feature

--- src/keyboard.js
+++ src/keyboard.js
@@ -2,14 +2,15 @@ import {remove} from './actions/editing-actions';
 import {MODE_IDLE} from './constants';
 
 const KEY_DELETE = 46;
+const KEY_BACKSPACE = 8;
 
 export default function keyboard(store) {
 
   window.addEventListener('keydown', event => {
     switch (event.keyCode) {
+      case KEY_BACKSPACE:
       case KEY_DELETE:
-        let state = store.getState();
-        if (state.mode === MODE_IDLE) store.dispatch(remove());
+        if (store.getState().mode === MODE_IDLE) store.dispatch(remove());
         break;
     }
   });
