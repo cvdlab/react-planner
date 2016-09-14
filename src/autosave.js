41d1338dca1af527f02f9322af4472d41df4ff9b
Christian Vadalà <chr.vadala@gmail.com>
2016-09-14 18:30:13 +0200

    enhances autosave feature

--- src/autosave.js
+++ src/autosave.js
@@ -1,7 +1,14 @@
 import {loadProject} from './actions/project-actions';
+import * as constants from './constants';
 
 const storageKey = 'metior_scene_autosave_v1';
 const localStorage = window.hasOwnProperty('localStorage') ? window.localStorage : false;
+const SAFE_MODES = [
+  constants.MODE_IDLE,
+  constants.MODE_3D_VIEW,
+  constants.MODE_3D_FIRST_PERSON,
+  constants.MODE_WAITING_DRAWING_LINE
+];
 
 export default function autosave(store) {
 
@@ -16,9 +23,11 @@ export default function autosave(store) {
 
   //update
   store.subscribe(() => {
-    let {scene} = store.getState();
-    let json = JSON.stringify(scene.toJSON());
-    localStorage.setItem(storageKey, json);
+    let {scene, mode} = store.getState();
+    if (SAFE_MODES.includes(mode)) {
+      let json = JSON.stringify(scene.toJSON());
+      localStorage.setItem(storageKey, json);
+    }
   });
 
 }
