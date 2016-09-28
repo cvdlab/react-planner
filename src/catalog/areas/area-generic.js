79d9dca159f7918e7921d5f8ba64640c89e4fb0a
Christian Vadalà <chr.vadala@gmail.com>
2016-09-28 15:47:44 +0200

    avoid 2d jsx catalog element

--- src/catalog/areas/area-generic.js
+++ src/catalog/areas/area-generic.js
@@ -1,4 +1,6 @@
-import render2D from './area-generic.2d.jsx';
+import React from 'react';
+
+let pathSVG = React.createFactory('path');
 import render3D from './area-generic.3d';
 
 export default {
@@ -27,7 +29,21 @@ export default {
     }
   },
 
-  render2D,
+  render2D: function (element, layer, scene) {
+    let path = "";
+    let first = true;
+
+    element.vertices.valueSeq()
+      .map(vertexID => layer.vertices.get(vertexID))
+      .forEach((vertex, vertexID) => {
+        path += `${first ? 'M' : 'L'} ${vertex.x} ${vertex.y} `;
+        first = false;
+      });
+
+    let fill = element.selected ? "#99c3fb" : element.properties.get('patternColor');
+
+    return pathSVG({d: path, fill});
+  },
 
   render3D,
 
