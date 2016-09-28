20b534f09d621a21c95a45e1bcf5828d935691b7
Christian Vadalà <chr.vadala@gmail.com>
2016-09-28 16:36:08 +0200

    enhances catalog item

--- src/components/catalog-view/catalog-item.jsx
+++ src/components/catalog-view/catalog-item.jsx
@@ -1,15 +1,19 @@
 import React, {PropTypes, Component} from 'react';
 import IconAdd from 'react-icons/lib/fa/plus-circle';
 import If from '../../utils/react-if.jsx';
+import {Seq} from 'immutable';
 
 const STYLE_BOX = {
   width: "200px",
   height: "200px",
   padding: "10px",
-  background: "#ccc",
+  background: "#f7f7f9",
+  border: "1px solid #e1e1e8",
   margin: "5px",
   cursor: "pointer",
-  position: "relative"
+  position: "relative",
+  boxShadow: "rgba(0, 0, 0, 0.117647) 0px 1px 6px, rgba(0, 0, 0, 0.117647) 0px 1px 4px",
+  borderRadius: "2px"
 };
 
 const STYLE_BOX_HOVER = {
@@ -21,16 +25,17 @@ const STYLE_IMAGE = {
   width: "200px",
   height: "130px",
   background: "#222",
-  marginBottom: "5px"
+  marginBottom: "5px",
+  border: "solid 1px #e6e6e6"
 };
 
 const STYLE_NAME = {
-  fontSize: "12px",
+  fontSize: "16px",
   fontWeight: "bold"
 };
 
 const STYLE_DESCRIPTION = {
-  fontSize: "11px",
+  fontSize: "12px",
 };
 
 const STYLE_ICON = {
@@ -47,6 +52,23 @@ const STYLE_SELECT = {
   width: "200px",
   fontSize: "30px",
   opacity: "0.7"
+};
+
+const STYLE_TAGS = {
+  listStyle: "none",
+  margin: "0px",
+  padding: "0px",
+  fontSize: "11px",
+  marginBottom: "3px"
+};
+
+const STYLE_TAG = {
+  display: "inline-block",
+  background: "#337ab7",
+  color: "#fff",
+  padding: "1px 4px",
+  marginRight: "3px",
+  borderRadius: "3px"
 };
 
 export default class CatalogItem extends Component {
@@ -87,6 +109,9 @@ export default class CatalogItem extends Component {
           <div style={STYLE_SELECT}><IconAdd /></div>
         </If>
         <div style={STYLE_NAME}>{element.name}</div>
+        <ul style={STYLE_TAGS}>
+          {new Seq(element.info.tag).map(tag => <li style={STYLE_TAG}>{tag}</li>)}
+        </ul>
         <div style={STYLE_DESCRIPTION}>{element.info.description}</div>
         <div style={STYLE_ICON}>
           <IconAdd />
