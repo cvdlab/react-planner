a6ebb64dabcf5756304963252ec0132744413a4e
Christian Vadalà <chr.vadala@gmail.com>
2016-10-12 13:07:00 +0200

    change renderer.js location

--- demo/renderer.js
+++ demo/renderer.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Demo from './demo.jsx';
+import Demo from './src/demo.jsx';
 
 ReactDOM.render(
   React.createElement(Demo),
   document.getElementById('demo')
 );
-
-
-
 
