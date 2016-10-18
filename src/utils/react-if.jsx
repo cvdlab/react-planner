452d2156d65d9856b51e5516c893d985c5ab03ff
Christian Vadalà <chr.vadala@gmail.com>
2016-10-18 15:15:23 +0200

    enhances react-if

--- src/utils/react-if.jsx
+++ src/utils/react-if.jsx
@@ -4,7 +4,6 @@ import React, {PropTypes} from 'react';
  * @return {null}
  */
 export default function If({condition, children}) {
-  children = <div>{children}</div>;
   return condition ? children : null;
 }
 
