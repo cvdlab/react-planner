96a4195ddce91ed7b85fd3f8b9c953014f0016c5
Christian Vadalà <chr.vadala@gmail.com>
2016-10-18 18:10:21 +0200

    export ruler

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import Planner from './components/planner';
 import Catalog from './catalog/catalog';
 import ToolbarButton from './components/toolbar/toolbar-button';
+import Ruler from './components/viewer2d/ruler';
 
 console.info("react-planner started"); //MIT LICENSE COMPLIANT
 
 
 export {
-  Planner, Catalog, ToolbarButton
+  Planner, Catalog, ToolbarButton, Ruler
 };
