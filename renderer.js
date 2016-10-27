07e1b24d1e02164765f45ae97ed5edf94ffa141f
Christian Vadalà <chr.vadala@gmail.com>
2016-10-27 14:29:50 +0200

    Updates

--- renderer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Demo from './src/demo.jsx';
-
-ReactDOM.render(
-  React.createElement(Demo),
-  document.getElementById('demo')
-);
-
