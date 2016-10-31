fcdffe016a836a42b5a464aa30ced1949157c316
Christian Vadalà <chr.vadala@gmail.com>
2016-10-31 11:14:01 +0100

    update svg pan zoom

--- src/components/viewer2d/viewer2d.jsx
+++ src/components/viewer2d/viewer2d.jsx
@@ -2,7 +2,7 @@
 
 import React, {PropTypes} from 'react';
 
-import {Viewer, TOOL_NONE, TOOL_PAN, TOOL_ZOOM_IN, TOOL_ZOOM_OUT} from 'react-svg-pan-zoom';
+import {ReactSVGPanZoom, TOOL_NONE, TOOL_PAN, TOOL_ZOOM_IN, TOOL_ZOOM_OUT} from 'react-svg-pan-zoom';
 import * as constants from '../../constants';
 import Scene from './scene';
 import Snap from './snap';
@@ -254,7 +254,7 @@ export default function Viewer2D({state, width, height},
   snapElements = null; //only for debug purpose
 
   return (
-    <Viewer value={viewer2D.isEmpty() ? null : viewer2D.toJS()} tool={mode2Tool(mode)} width={width} height={height}
+    <ReactSVGPanZoom value={viewer2D.isEmpty() ? null : viewer2D.toJS()} tool={mode2Tool(mode)} width={width} height={height}
             detectAutoPan={mode2DetectAutopan(mode)}
             onMouseMove={onMouseMove} onChange={onChange} onClick={onClick} onMouseDown={onMouseDown}
             onMouseUp={onMouseUp}>
@@ -270,7 +270,7 @@ export default function Viewer2D({state, width, height},
         </g>
       </svg>
 
-    </Viewer>
+    </ReactSVGPanZoom>
   );
 }
 
