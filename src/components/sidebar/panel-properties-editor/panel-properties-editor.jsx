1410fd148b032e6ad151d45367e2b4452e065eab
Christian Vadalà <chr.vadala@gmail.com>
2016-11-16 11:37:17 +0100

    split property editor files

---
+++ src/components/sidebar/panel-properties-editor/panel-properties-editor.jsx
@@ -0,0 +1,33 @@
+import React, {PropTypes, Component} from 'react';
+import Panel from '../panel';
+import {Map, Seq, Iterable} from 'immutable';
+import {MODE_IDLE, MODE_3D_VIEW, MODE_3D_FIRST_PERSON} from '../../../constants';
+import PropertiesEditor from './properties-editor';
+
+export default function PanelPropertiesEditor({state: {scene, mode}}) {
+
+  if (![MODE_IDLE, MODE_3D_VIEW, MODE_3D_FIRST_PERSON].includes(mode)) return null;
+
+  let componentRenderer = (element, layer) =>
+    <Panel key={element.id} name={`Properties: [${element.type}] ${element.id}`}>
+      <div style={{padding: "5px 15px 5px 15px"}}>
+        <PropertiesEditor element={element} layer={layer}/>
+      </div>
+    </Panel>;
+
+  let layerRenderer = layer => Seq()
+    .concat(layer.lines)
+    .concat(layer.holes)
+    .concat(layer.areas)
+    .concat(layer.items)
+    .filter(element => element.selected)
+    .map(element => componentRenderer(element, layer))
+    .valueSeq();
+
+  return <div>{scene.layers.valueSeq().map(layerRenderer)}</div>
+
+}
+
+PanelPropertiesEditor.propTypes = {
+  state: PropTypes.object.isRequired,
+};
--- src/components/sidebar/panel-properties-editor/properties-editor.jsx
+++ src/components/sidebar/panel-properties-editor/properties-editor.jsx
@@ -1,38 +1,6 @@
 import React, {PropTypes, Component} from 'react';
-import Panel from './panel';
-import {Map, Seq, Iterable} from 'immutable';
-import {MODE_IDLE, MODE_3D_VIEW, MODE_3D_FIRST_PERSON} from '../../constants';
+import {Seq, Iterable} from 'immutable';
 
-export default function PanelPropertiesEditor({state: {scene, mode}}) {
-
-  if (![MODE_IDLE, MODE_3D_VIEW, MODE_3D_FIRST_PERSON].includes(mode)) return null;
-
-  let componentRenderer = (element, layer) =>
-    <Panel key={element.id} name={`Properties: [${element.type}] ${element.id}`}>
-      <div style={{padding: "5px 15px 5px 15px"}}>
-        <PropertiesEditor element={element} layer={layer}/>
-      </div>
-    </Panel>;
-
-  let layerRenderer = layer => Seq()
-    .concat(layer.lines)
-    .concat(layer.holes)
-    .concat(layer.areas)
-    .concat(layer.items)
-    .filter(element => element.selected)
-    .map(element => componentRenderer(element, layer))
-    .valueSeq();
-
-  return <div>{scene.layers.valueSeq().map(layerRenderer)}</div>
-
-}
-
-PanelPropertiesEditor.propTypes = {
-  state: PropTypes.object.isRequired,
-};
-
-
-/**** PROPERTIES EDITOR ****/
 const STYLE_WRAPPER_BUTTONS = {textAlign: "right"};
 const STYLE_BUTTON_UNSELECT = {backgroundColor: "gray", border: 0, color: "white", margin: "3px"};
 const STYLE_BUTTON_RESET = {backgroundColor: "gray", border: 0, color: "white", margin: "3px"};
