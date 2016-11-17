b3be8fecb0029581c4c31d4dbca980ad40194a11
Christian Vadalà <chr.vadala@gmail.com>
2016-11-17 17:22:07 +0100

    add Models to public elements

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import Planner from './components/planner';
 import Catalog from './catalog/catalog';
 import ToolbarButton from './components/toolbar/toolbar-button';
 import Ruler from './components/viewer2d/ruler';
+import * as Models from './models'
 
 console.info(`react-planner started`); //MIT LICENSE COMPLIANT
 
 
 export {
-  Planner, Catalog, ToolbarButton, Ruler
+  Planner, Catalog, ToolbarButton, Ruler, Models
 };
