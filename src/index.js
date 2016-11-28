0f9b7bec84dd96150c4c65226fa1f4a924e45a1f
Christian Vadalà <chr.vadala@gmail.com>
2016-11-28 18:11:34 +0100

    expose State2DViewer

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import ToolbarButton from './components/toolbar/toolbar-button';
 import Ruler from './components/viewer2d/ruler';
 import * as Models from './models'
 import PluginBrowserAutosave from './plugins/browser-autosave/browser-autosave';
+import State2DViewer from './components/viewer2d/state';
 
 console.info(`react-planner started`); //MIT LICENSE COMPLIANT
 
 export {
-  Planner, Catalog, ToolbarButton, Ruler, Models, PluginBrowserAutosave
+  Planner, Catalog, ToolbarButton, Ruler, Models, PluginBrowserAutosave, State2DViewer
 };
