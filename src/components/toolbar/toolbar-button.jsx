9178ed4871552435c68bba7fd8fe3a5f7b2178e4
Christian Vadalà <chr.vadala@gmail.com>
2017-01-04 18:59:19 +0100

    add toolbar tooltip support

--- src/components/toolbar/toolbar-button.jsx
+++ src/components/toolbar/toolbar-button.jsx
@@ -1,5 +1,7 @@
 import React, {Component, PropTypes} from 'react';
 
+//http://www.cssportal.com/css-tooltip-generator/
+
 const STYLE = {
   width: "30px",
   height: "30px",
@@ -7,9 +9,40 @@ const STYLE = {
   alignItems: "center",
   justifyContent: "center",
   marginBottom: "5px",
-  fontSize: "25px"
+  fontSize: "25px",
+  position: "relative",
 };
 
+const STYLE_TOOLTIP = {
+  position: "absolute",
+  width: "140px",
+  color: "#FFFFFF",
+  background: "#000000",
+  height: "30px",
+  lineHeight: "30px",
+  textAlign: "center",
+  visibility: "visible",
+  borderRadius: "6px",
+  opacity: "0.8",
+  left: "100%",
+  top: "50%",
+  marginTop: "-15px",
+  marginLeft: "15px",
+  zIndex: "999",
+  fontSize: "12px",
+};
+
+const STYLE_TOOLTIP_PIN = {
+  position: "absolute",
+  top: "50%",
+  right: "100%",
+  marginTop: "-8px",
+  width: "0",
+  height: "0",
+  borderRight: "8px solid #000000",
+  borderTop: "8px solid transparent",
+  borderBottom: "8px solid transparent"
+};
 
 export default class ToolbarButton extends Component {
 
@@ -27,9 +60,18 @@ export default class ToolbarButton extends Component {
            onMouseEnter={event => this.setState({active: true})}
            onMouseLeave={event => this.setState({active: false})}>
 
-        <a href="javascript:;" style={{color, textDecoration: "none"}} onClick={props.onClick} title={props.tooltip}>
+        <a href="javascript:;" style={{color, textDecoration: "none"}} onClick={props.onClick}>
           {props.children}
         </a>
+
+        {state.active ?
+          <div style={STYLE_TOOLTIP}>
+            <span style={STYLE_TOOLTIP_PIN}/>
+            {props.tooltip}
+          </div>
+          : null
+        }
+
       </div>
     )
   }
@@ -39,4 +81,4 @@ ToolbarButton.propTypes = {
   active: PropTypes.bool.isRequired,
   onClick: PropTypes.func.isRequired,
   tooltip: PropTypes.string.isRequired
-}
+};
