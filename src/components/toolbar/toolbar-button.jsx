36be8217d36a7dc90a88792cc4cc9b4b9d9d1610
Christian Vadalà <chr.vadala@gmail.com>
2017-01-04 18:50:41 +0100

    add toolbar hover support

--- src/components/toolbar/toolbar-button.jsx
+++ src/components/toolbar/toolbar-button.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component, PropTypes} from 'react';
 
 const STYLE = {
   width: "30px",
@@ -11,15 +11,32 @@ const STYLE = {
 };
 
 
-export default function ToolbarButton(props) {
+export default class ToolbarButton extends Component {
 
-  let color = props.active === true ? '#1CA6FC' : '#C2C2C2';
+  constructor(props, context) {
+    super(props, context);
+    this.state = {active: false};
+  }
 
-  return (
-    <div style={STYLE}>
-      <a href="javascript:;" style={{color, textDecoration: "none"}} onClick={props.onClick} title={props.tooltip}>
-        {props.children}
-      </a>
-    </div>
-  )
+  render() {
+    let {state, props} = this;
+    let color = props.active || state.active ? '#1CA6FC' : '#C2C2C2';
+
+    return (
+      <div style={STYLE}
+           onMouseEnter={event => this.setState({active: true})}
+           onMouseLeave={event => this.setState({active: false})}>
+
+        <a href="javascript:;" style={{color, textDecoration: "none"}} onClick={props.onClick} title={props.tooltip}>
+          {props.children}
+        </a>
+      </div>
+    )
+  }
+}
+
+ToolbarButton.propTypes = {
+  active: PropTypes.bool.isRequired,
+  onClick: PropTypes.func.isRequired,
+  tooltip: PropTypes.string.isRequired
 }
