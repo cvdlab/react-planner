ece4eee11da45466df269c827f4e109eec3edaf0
Alessio Carrafa <alessio.carrafa@hotmail.it>
2017-03-10 13:42:00 +0100

    added orderVertices

--- src/utils/geometry.js
+++ src/utils/geometry.js
@@ -2,27 +2,38 @@ export function compareVertices(v0, v1) {
   return v0.x === v1.x ? v0.y - v1.y : v0.x - v1.x
 }
 
-export function minVertex(v0,v1) {
-  return compareVertices(v0,v1) > 0 ? v1: v0;
+export function minVertex(v0, v1) {
+  return compareVertices(v0, v1) > 0 ? v1 : v0;
 }
 
-export function maxVertex(v0,v1) {
-  return compareVertices(v0,v1) > 0 ? v0: v1;
+export function maxVertex(v0, v1) {
+  return compareVertices(v0, v1) > 0 ? v0 : v1;
 }
 
+export function orderVertices(vertices){
+  return vertices.sort( compareVertices );
+}
+
+/** @description Determines the distance between two points
+ *  @param {number} x0 Vertex 0 x
+ *  @param {number} y0 Vertex 0 y
+ *  @param {number} x1 Vertex 1 x
+ *  @param {number} y1 Vertex 1 y
+ *  @return {number}
+*/
 export function pointsDistance(x0, y0, x1, y1) {
   let diff_x = x0 - x1;
   let diff_y = y0 - y1;
 
-  return Math.sqrt( ( diff_x * diff_x ) + ( diff_y * diff_y ) );
+  return Math.sqrt((diff_x * diff_x) + (diff_y * diff_y));
 }
 
 export function horizontalLine(y) {
-  return {a: 0, b: 1, c: -y}
+  return { a: 0, b: 1, c: -y }
 }
 
 export function verticalLine(x) {
-  return {a: 1, b: 0, c: -x}
+  return { a: 1, b: 0, c: -x }
 }
 
 export function linePassingThroughTwoPoints(x1, y1, x2, y2) {
@@ -50,7 +61,7 @@ export function closestPointFromLine(a, b, c, x, y) {
   let denom = a * a + b * b;
   return {
     x: (b * (b * x - a * y) - a * c) / denom,
-    y: ((a * -b * x + a * y ) - b * c) / denom,
+    y: ((a * -b * x + a * y) - b * c) / denom,
   }
 }
 
@@ -61,16 +72,16 @@ export function intersectionFromTwoLines(a, b, c, j, k, l) {
 
   let y = (a * l - c * j) / det;
   let x = (c * k - b * l) / det;
-  return {x, y};
+  return { x, y };
 }
 
 export function intersectionFromTwoLineSegment(p1, p2, p3, p4) {
   //https://github.com/psalaets/line-intersect/blob/master/lib/check-intersection.js
 
-  let {x: x1, y: y1} = p1;
-  let {x: x2, y: y2} = p2;
-  let {x: x3, y: y3} = p3;
-  let {x: x4, y:y4} = p4;
+  let { x: x1, y: y1 } = p1;
+  let { x: x2, y: y2 } = p2;
+  let { x: x3, y: y3 } = p3;
+  let { x: x4, y: y4 } = p4;
 
   let EPSILON = 10e-6;
 
@@ -88,12 +99,12 @@ export function intersectionFromTwoLineSegment(p1, p2, p3, p4) {
       let [lineSX, lineDX] = [line0, line1].sort((lineA, lineB) => comparator(lineA[0], lineB[0]));
 
       if (lineSX[1].x === lineDX[0].x) {
-        return {type: (lineDX[0].y <= lineSX[1].y) ? "colinear" : "none"};
+        return { type: (lineDX[0].y <= lineSX[1].y) ? "colinear" : "none" };
       } else {
-        return {type: (lineDX[0].x <= lineSX[1].x) ? "colinear" : "none"};
+        return { type: (lineDX[0].x <= lineSX[1].x) ? "colinear" : "none" };
       }
     }
-    return {type: "parallel"};
+    return { type: "parallel" };
   }
 
   var uA = numA / denom;
@@ -104,10 +115,10 @@ export function intersectionFromTwoLineSegment(p1, p2, p3, p4) {
       x: x1 + (uA * (x2 - x1)),
       y: y1 + (uA * (y2 - y1))
     };
-    return {type: "intersecting", point};
+    return { type: "intersecting", point };extendLine
   }
 
-  return {type: "none"};
+  return { type: "none" };
 }
 
 export function distancePointFromLineSegment(x1, y1, x2, y2, xp, yp) {
@@ -145,8 +156,8 @@ export function distancePointFromLineSegment(x1, y1, x2, y2, xp, yp) {
 }
 
 export function closestPointFromLineSegment(x1, y1, x2, y2, xp, yp) {
-  if (x1 === x2) return {x: x1, y: yp};
-  if (y1 === y2) return {x: xp, y: y1};
+  if (x1 === x2) return { x: x1, y: yp };
+  if (y1 === y2) return { x: xp, y: y1 };
 
   let m = (y2 - y1) / (x2 - x1);
   let q = y1 - m * x1;
@@ -157,7 +168,7 @@ export function closestPointFromLineSegment(x1, y1, x2, y2, xp, yp) {
   let x = (qi - q) / (m - mi);
   let y = (m * x + q);
 
-  return {x, y};
+  return { x, y };
 }
 
 export function pointPositionOnLineSegment(x1, y1, x2, y2, xp, yp) {
@@ -179,7 +190,24 @@ export function angleBetweenTwoPointsAndOrigin(x1, y1, x2, y2) {
   return (-Math.asin((y1 - y2) / length)) * 180 / Math.PI;
 }
 
-export function samePoints({x:x1, y:y1}, {x:x2, y:y2}) {
+export function samePoints({ x: x1, y: y1 }, { x: x2, y: y2 }) {
   let EPSILON = 10e-6;
   return Math.abs(x1 - x2) <= EPSILON && Math.abs(y1 - y2) <= EPSILON;
+}
+
+/** @description Extend line based on coordinates and new line length
+ *  @param {number} x1 Vertex 1 x
+ *  @param {number} y1 Vertex 1 y
+ *  @param {number} x2 Vertex 2 x
+ *  @param {number} y2 Vertex 2 y
+ *  @param {number} newDistance New line length
+ *  @return {object}
+*/
+export function extendLine(x1, y1, x2, y2, newDistance){
+  let rad = Math.atan2( y2 - y1, x2 - x1 );
+
+  return {
+    x: ( x1 + ( Math.cos( rad ) * newDistance ) ),
+    y: ( y1 + ( Math.sin( rad ) * newDistance ) ),
+  }
 }
