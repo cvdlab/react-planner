a41dd65cde27ec2bf1a6e87567ff81fbb24079dd
Alessio Carrafa <alessio.carrafa@hotmail.it>
2017-03-17 19:11:02 +0100

    added min and max check

--- src/components/style/form-number-input.jsx
+++ src/components/style/form-number-input.jsx
@@ -1,25 +1,42 @@
 import React, {PropTypes, Component} from 'react';
 import FormTextInput from './form-text-input';
 
+//TODO Check if min and max are not defined
+
 export default function FormNumberInput({onChange, ...rest}) {
   let onChangeCustom = event => {
     let value = event.target.value;
 
-    if( !isNaN(parseFloat(value)) && isFinite(value) )
+    if( value <= rest.min || value >= rest.max )
     {
+      event.target.value = rest.min;
       onChange(event);
+      event.target.select();
     }
     else
     {
-      if( value === '-' )
+      if( !isNaN(parseFloat(value)) && isFinite(value) )
       {
         onChange(event);
       }
-      else if( value === '0' || value === '' )
+      else
       {
-        event.target.value = '0';
-        onChange(event);
-        event.target.select();
+        if( value === '-' )
+        {
+          if( rest.min <= 0 )
+          {
+            event.target.value = 0;
+            event.target.select();
+            onChange(event);
+          }
+        }
+        else if( value === '0' || value === '' )
+        {
+          event.target.value = rest.min;
+          onChange(event);
+          event.target.select();
+        }
+
       }
     }
   };
