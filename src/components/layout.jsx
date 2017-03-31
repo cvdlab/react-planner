6b18d22846c7124050b3ae9e3c4f46a2330640fa
Alessio Carrafa <alessio.carrafa@hotmail.it>
2017-03-31 11:20:51 +0200

    fixed scroll interceptor

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -10,9 +10,9 @@ export default function ({width, height, ...rest}) {
   let contentWidth = width - toolbarWidth - sidebarWidth;
 
   return (
-    <div style={{display: "flex", flexFlow: "row nowrap", height}} onWheel={event => event.preventDefault()}>
+    <div style={{display: "flex", flexFlow: "row nowrap", height}}>
       <Toolbar width={toolbarWidth} height={height} {...rest} />
-      <Content width={contentWidth} height={height} {...rest} />
+      <Content width={contentWidth} height={height} {...rest} onWheel={event => event.preventDefault()} />
       <Sidebar width={sidebarWidth} height={height} {...rest} />
     </div>
   );
