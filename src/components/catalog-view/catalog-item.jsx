c84a471b5cde5bee21ddb92c1472ba8eff2c1bd6
Alessio Carrafa <alessio.carrafa@hotmail.it>
2017-04-11 13:13:51 +0200

    code cleaning, ui improved

--- src/components/catalog-view/catalog-item.jsx
+++ src/components/catalog-view/catalog-item.jsx
@@ -4,15 +4,15 @@ import If from '../../utils/react-if';
 import {Seq} from 'immutable';
 
 const STYLE_BOX = {
-  width: "220px",
-  height: "220px",
-  padding: "10px",
+  width: "13.75em",
+  height: "13.75em",
+  padding: "0.625em",
   background: "#f7f7f9",
   border: "1px solid #e1e1e8",
-  margin: "5px",
+  margin: "0.3em",
   cursor: "pointer",
   position: "relative",
-  boxShadow: "rgba(0, 0, 0, 0.117647) 0px 1px 6px, rgba(0, 0, 0, 0.117647) 0px 1px 4px",
+  boxShadow: "0 1px 6px 0 rgba(0, 0, 0, 0.11), 0 1px 4px 0 rgba(0, 0, 0, 0.11)",
   borderRadius: "2px"
 };
 
@@ -22,36 +22,33 @@ const STYLE_BOX_HOVER = {
 };
 
 const STYLE_IMAGE = {
-  width: "200px",
-  height: "130px",
   background: "#222",
   marginBottom: "5px",
-  border: "solid 1px #e6e6e6"
+  border: "solid 1px #e6e6e6",
+  width: '100%',
+  height: '8em',
+  backgroundSize: 'contain',
+  backgroundPosition:'50% 50%',
+  backgroundColor:'#FFF',
+  backgroundRepeat:'no-repeat'
 };
 
-const STYLE_TITLE = {
-  fontSize: "16px",
-  fontWeight: "bold"
+const STYLE_PLUS_HOVER = {
+  marginTop:'1.5em',
+  color: "#1ca6fc",
+  fontSize: "2em",
+  opacity: "0.7",
+  width: '100%'
 };
 
 const STYLE_DESCRIPTION = {
-  fontSize: "12px",
+  fontSize: "0.75em",
 };
 
 const STYLE_ICON = {
   position: "absolute",
   bottom: "5px",
   right: "10px"
-};
-
-const STYLE_SELECT = {
-  position: "absolute",
-  top: "55px",
-  color: "#1ca6fc",
-  textAlign: "center",
-  width: "200px",
-  fontSize: "30px",
-  opacity: "0.7"
 };
 
 const STYLE_TAGS = {
@@ -79,7 +76,7 @@ export default class CatalogItem extends Component {
   }
 
   select() {
-    let {element} = this.props;
+    let element = this.props.element;
     let {linesActions, holesActions, itemsActions} = this.context;
 
     switch (element.prototype) {
@@ -96,26 +93,23 @@ export default class CatalogItem extends Component {
   }
 
   render() {
-    let {element} = this.props;
-    let {hover} = this.state;
+    let element = this.props.element;
+    let hover = this.state.hover;
 
     return (
       <div style={hover ? STYLE_BOX_HOVER : STYLE_BOX}
-           onClick={event => this.select()}
+           onClick={e => this.select()}
            onMouseEnter={e => this.setState({hover: true})}
            onMouseLeave={e => this.setState({hover: false})}>
-        <img style={STYLE_IMAGE} src={element.info.image}/>
-        <If condition={hover}>
-          <div style={STYLE_SELECT}><IconAdd /></div>
-        </If>
-        <div style={STYLE_TITLE}>{element.info.title}</div>
+        <div style={{ ...STYLE_IMAGE, backgroundImage: 'url(' + element.info.image + ')'}}>
+          { hover ? <IconAdd style={STYLE_PLUS_HOVER} /> : null }
+        </div>
+        <b>{element.info.title}</b>
         <ul style={STYLE_TAGS}>
           {new Seq(element.info.tag).map((tag, index) => <li style={STYLE_TAG} key={index}>{tag}</li>)}
         </ul>
         <div style={STYLE_DESCRIPTION}>{element.info.description}</div>
-        <div style={STYLE_ICON}>
-          <IconAdd />
-        </div>
+        <IconAdd style={STYLE_ICON}/>
       </div>
     );
   }
