d88714c65e7fd5b42e689093ef27308bd93c3d24
Alessio Carrafa <alessio.carrafa@hotmail.it>
2017-05-19 17:24:15 +0200

    temporary fix for 32bit bitwise operation on toFixedFloat

--- src/utils/math.js
+++ src/utils/math.js
@@ -1,3 +1,16 @@
+/** @description Return float fixed to desired precision, > 32 bit error
+ *  @param {number} num Float to fix
+ *  @param {number} precision Desired precision, or 6 if not specified
+ *  @return {number}
+*/
+export function _toFixedFloat(num, precision = 6) {
+  if (num && precision) {
+    let prec = (Math.pow(10,precision));
+    return ( ~~(num * prec) / prec);
+  }
+  return 0;
+}
+
 /** @description Return float fixed to desired precision
  *  @param {number} num Float to fix
  *  @param {number} precision Desired precision, or 6 if not specified
@@ -5,8 +18,7 @@
 */
 export function toFixedFloat(num, precision = 6) {
   if (num && precision) {
-    let prec = (Math.pow(10,precision));
-    return ( ~~(num * prec) / prec);
+    return parseFloat( parseFloat( num ).toFixed( precision ) );
   }
   return 0;
 }
