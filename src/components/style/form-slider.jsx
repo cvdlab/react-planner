726be9d3a9253f1c56ca25041437a89f0d469a75
Alessio Carrafa <alessio.carrafa@hotmail.it>
2017-06-20 18:43:10 +0200

    style fixing, code cleaning

--- src/components/style/form-slider.jsx
+++ src/components/style/form-slider.jsx
@@ -2,22 +2,20 @@ import React from 'react';
 import ReactRange from '@mapbox/react-range';
 import FormTextInput from './form-text-input';
 
-const STYLE_INPUT = {
-  display: "block",
-  width: "100%",
-  height: "30px",
-};
-
+const sliderContainerStyle = {display: 'inline-block', width: '80%', marginRight: '5%'};
+const sliderStyle = { display: 'block', width: '100%', height: '30px' };
+const textContainerStyle = {display: 'inline-block', width: '15%', float: 'right'};
+const textStyle = {height:'34px', textAlign:'center'};
 
 export default function FormNumberInput({value, onChange, ...rest}) {
   return (
     <div>
-      <div style={{display: "inline-block", width: "85%", marginRight: "5%"}}>
-        <ReactRange type="range" style={STYLE_INPUT} onChange={onChange} value={value} {...rest}/>
+      <div style={sliderContainerStyle}>
+        <ReactRange type='range' style={sliderStyle} onChange={onChange} value={value} {...rest}/>
       </div>
 
-      <div style={{display: "inline-block", width: "10%"}}>
-        <FormTextInput value={value} onChange={onChange}/>
+      <div style={textContainerStyle}>
+        <FormTextInput value={value} onChange={onChange} style={textStyle}/>
       </div>
     </div>
   )
