200c03d7d0b97423155980381c4663823884ba28
Alessio Carrafa <alessio.carrafa@hotmail.it>
2017-06-30 15:57:37 +0200

    Alt key now launching "snap toggle" action

--- src/plugins/keyboard.js
+++ src/plugins/keyboard.js
@@ -1,10 +1,11 @@
-import {MODE_IDLE, MODE_3D_FIRST_PERSON, MODE_3D_VIEW} from '../constants';
-import {rollback, undo, remove} from '../actions/project-actions';
+import { MODE_IDLE, MODE_3D_FIRST_PERSON, MODE_3D_VIEW } from '../constants';
+import { rollback, undo, remove, toggleSnap } from '../actions/project-actions';
 
 const KEY_DELETE = 46;
 const KEY_BACKSPACE = 8;
 const KEY_ESC = 27;
 const KEY_Z = 90;
+const KEY_ALT = 18;
 
 export default function keyboard() {
 
@@ -17,19 +18,42 @@ export default function keyboard() {
       switch (event.keyCode) {
         case KEY_BACKSPACE:
         case KEY_DELETE:
-          if ([MODE_IDLE, MODE_3D_FIRST_PERSON, MODE_3D_VIEW].includes(state.get('mode')))
-            store.dispatch(remove());
-          break;
-
+          {
+            if ([MODE_IDLE, MODE_3D_FIRST_PERSON, MODE_3D_VIEW].includes(state.get('mode')))
+              store.dispatch(remove());
+            break;
+          }
         case KEY_ESC:
-          store.dispatch(rollback());
-          break;
-
+          {
+            store.dispatch(rollback());
+            break;
+          }
         case KEY_Z:
-          if (event.getModifierState('Control') || event.getModifierState('Meta'))
-            store.dispatch(undo());
-          break;
+          {
+            if (event.getModifierState('Control') || event.getModifierState('Meta'))
+              store.dispatch(undo());
+            break;
+          }
+        case KEY_ALT:
+          {
+            store.dispatch(toggleSnap(0));
+            break;
+          }
       }
+
     });
+
+    window.addEventListener('keyup', event => {
+
+      switch (event.keyCode) {
+        case KEY_ALT:
+          {
+            store.dispatch(toggleSnap(1));
+            break;
+          }
+      }
+
+    });
+
   }
 }
