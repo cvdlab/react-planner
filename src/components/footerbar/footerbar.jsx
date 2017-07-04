3d6fdbe8323387d46c392fed235ed90914449bdd
Alessio Carrafa <alessio.carrafa@hotmail.it>
2017-07-04 17:10:42 +0200

    added conditional visualization on footerbar "widget", code cleaning, style fixing

--- src/components/footerbar/footerbar.jsx
+++ src/components/footerbar/footerbar.jsx
@@ -1,18 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import IconNewFile from 'react-icons/lib/fa/file-o';
-import IconPointer from 'react-icons/lib/fa/mouse-pointer';
-import IconZoomPlus from 'react-icons/lib/ti/zoom-in';
-import IconZoomMinus from 'react-icons/lib/ti/zoom-out';
-import IconPan from 'react-icons/lib/fa/hand-paper-o';
-import Icon3DFirstPerson from 'react-icons/lib/md/directions-run';
-import IconCatalog from 'react-icons/lib/fa/plus';
-import IconUndo from 'react-icons/lib/md/undo';
-import IconConfigure from 'react-icons/lib/md/settings';
 import If from '../../utils/react-if';
-import { VERSION } from '../../version';
 import FooterToggleButton from './footer-toggle-button';
+import { VERSION } from '../../version';
 import { SNAP_POINT, SNAP_LINE, SNAP_SEGMENT, SNAP_MASK } from '../../utils/snap';
+import { MODE_SNAPPING } from '../../constants';
 
 const footerBarStyle = {
   position: 'absolute',
@@ -27,7 +19,8 @@ const footerBarStyle = {
   margin: 0,
   boxSizing: 'border-box',
   cursor: 'default',
-  userSelect: 'none'
+  userSelect: 'none',
+  zIndex: '9001'
 };
 
 export const leftTextStyle = {
@@ -64,45 +57,48 @@ export default class FooterBar extends Component {
 
     let { x, y } = this.props.state.get('mouse').toJS();
     let zoom = this.props.state.get('zoom');
+    let mode = this.props.state.get('mode');
 
     return (
       <div style={footerBarStyle}>
 
-        <div style={leftTextStyle} title="Mouse Coordinates">
-          <div style={coordStyle}>X : {x.toFixed(3)}</div>
-          <div style={coordStyle}>Y : {y.toFixed(3)}</div>
-        </div>
+        <If condition={MODE_SNAPPING.includes(mode)}>
+          <div style={leftTextStyle}>
+            <div title="Mouse X Coordinates" style={coordStyle}>X : {x.toFixed(3)}</div>
+            <div title="Mouse Y Coordinates" style={coordStyle}>Y : {y.toFixed(3)}</div>
+          </div>
 
-        <div style={leftTextStyle} title="Scene Zoom Level">Zoom: {zoom.toFixed(3)}X</div>
+          <div style={leftTextStyle} title="Scene Zoom Level">Zoom: {zoom.toFixed(3)}X</div>
+
+          <div style={leftTextStyle}>
+            <FooterToggleButton
+              state={this.state}
+              toggleOn={() => { this.context.projectActions.toggleSnap(this.props.state.snapMask.merge({SNAP_POINT:true})) }}
+              toggleOff={() => { this.context.projectActions.toggleSnap(this.props.state.snapMask.merge({SNAP_POINT:false})) }}
+              text="Snap PT"
+              toggleState={this.props.state.snapMask.get(SNAP_POINT)}
+              title="Snap to Point"
+            />
+            <FooterToggleButton
+              state={this.state}
+              toggleOn={() => { this.context.projectActions.toggleSnap(this.props.state.snapMask.merge({SNAP_LINE:true})) }}
+              toggleOff={() => { this.context.projectActions.toggleSnap(this.props.state.snapMask.merge({SNAP_LINE:false})) }}
+              text="Snap LN"
+              toggleState={this.props.state.snapMask.get(SNAP_LINE)}
+              title="Snap to Line"
+            />
+            <FooterToggleButton
+              state={this.state}
+              toggleOn={() => { this.context.projectActions.toggleSnap(this.props.state.snapMask.merge({SNAP_SEGMENT:true})) }}
+              toggleOff={() => { this.context.projectActions.toggleSnap(this.props.state.snapMask.merge({SNAP_SEGMENT:false})) }}
+              text="Snap SEG"
+              toggleState={this.props.state.snapMask.get(SNAP_SEGMENT)}
+              title="Snap to Segment"
+            />
+          </div>
+        </If>
 
         {this.props.footerbarComponents.map((Component, index) => <Component state={state} key={index} />)}
-
-        <div id="footerToggleArea" style={leftTextStyle}>
-          <FooterToggleButton
-            state={this.state}
-            toggleOn={() => { this.context.projectActions.toggleSnap(this.props.state.snapMask.merge({SNAP_POINT:true})) }}
-            toggleOff={() => { this.context.projectActions.toggleSnap(this.props.state.snapMask.merge({SNAP_POINT:false})) }}
-            text="Snap PT"
-            toggleState={this.props.state.snapMask.get(SNAP_POINT)}
-            title="Snap to Point"
-          />
-          <FooterToggleButton
-            state={this.state}
-            toggleOn={() => { this.context.projectActions.toggleSnap(this.props.state.snapMask.merge({SNAP_LINE:true})) }}
-            toggleOff={() => { this.context.projectActions.toggleSnap(this.props.state.snapMask.merge({SNAP_LINE:false})) }}
-            text="Snap LN"
-            toggleState={this.props.state.snapMask.get(SNAP_LINE)}
-            title="Snap to Line"
-          />
-          <FooterToggleButton
-            state={this.state}
-            toggleOn={() => { this.context.projectActions.toggleSnap(this.props.state.snapMask.merge({SNAP_SEGMENT:true})) }}
-            toggleOff={() => { this.context.projectActions.toggleSnap(this.props.state.snapMask.merge({SNAP_SEGMENT:false})) }}
-            text="Snap SEG"
-            toggleState={this.props.state.snapMask.get(SNAP_SEGMENT)}
-            title="Snap to Segment"
-          />
-        </div>
 
         <div style={rightTextStyle}>React-Planner {VERSION}</div>
 
