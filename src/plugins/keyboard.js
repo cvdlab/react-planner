f4f862b9607b39437256733d0ef0ba41be3e1f93
Alessio Carrafa <alessio.carrafa@hotmail.it>
2017-07-04 11:45:31 +0200

    added MODE_SNAPPING and interceptor on keyboard plugin

--- src/plugins/keyboard.js
+++ src/plugins/keyboard.js
@@ -1,4 +1,4 @@
-import { MODE_IDLE, MODE_3D_FIRST_PERSON, MODE_3D_VIEW } from '../constants';
+import { MODE_IDLE, MODE_3D_FIRST_PERSON, MODE_3D_VIEW, MODE_SNAPPING } from '../constants';
 import { rollback, undo, remove, toggleSnap } from '../actions/project-actions';
 import { SNAP_POINT, SNAP_LINE, SNAP_SEGMENT, SNAP_MASK } from '../utils/snap';
 
@@ -15,31 +15,33 @@ export default function keyboard() {
     window.addEventListener('keydown', event => {
 
       let state = stateExtractor(store.getState());
+      let mode = state.get('mode');
 
       switch (event.keyCode) {
         case KEY_BACKSPACE:
         case KEY_DELETE:
-          {
-            if ([MODE_IDLE, MODE_3D_FIRST_PERSON, MODE_3D_VIEW].includes(state.get('mode')))
-              store.dispatch(remove());
-            break;
-          }
+        {
+          if ([MODE_IDLE, MODE_3D_FIRST_PERSON, MODE_3D_VIEW].includes(mode))
+            store.dispatch(remove());
+          break;
+        }
         case KEY_ESC:
-          {
-            store.dispatch(rollback());
-            break;
-          }
+        {
+          store.dispatch(rollback());
+          break;
+        }
         case KEY_Z:
-          {
-            if (event.getModifierState('Control') || event.getModifierState('Meta'))
-              store.dispatch(undo());
-            break;
-          }
+        {
+          if (event.getModifierState('Control') || event.getModifierState('Meta'))
+            store.dispatch(undo());
+          break;
+        }
         case KEY_ALT:
-          {
+        {
+          if( MODE_SNAPPING.includes(mode) )
             store.dispatch(toggleSnap(state.snapMask.merge({ SNAP_POINT: false, SNAP_LINE: false, SNAP_SEGMENT: false, tempSnapConfiguartion: state.snapMask.toJS() })));
-            break;
-          }
+          break;
+        }
       }
 
     });
@@ -47,11 +49,13 @@ export default function keyboard() {
     window.addEventListener('keyup', event => {
 
       let state = stateExtractor(store.getState());
+      let mode = state.get('mode');
 
       switch (event.keyCode) {
         case KEY_ALT:
           {
-            store.dispatch(toggleSnap(state.snapMask.merge(state.snapMask.get('tempSnapConfiguartion'))));
+            if( MODE_SNAPPING.includes(mode) )
+              store.dispatch(toggleSnap(state.snapMask.merge(state.snapMask.get('tempSnapConfiguartion'))));
             break;
           }
       }
