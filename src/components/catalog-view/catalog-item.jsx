be6aecf420a5acc63529cc46f50c956d2c969e07
Alessio Carrafa <alessio.carrafa@hotmail.it>
2017-07-21 13:19:33 +0200

    style fixing, performance improving

--- src/components/catalog-view/catalog-item.jsx
+++ src/components/catalog-view/catalog-item.jsx
@@ -1,31 +1,38 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import IconAdd from 'react-icons/lib/fa/plus-circle';
-import If from '../../utils/react-if';
 import {Seq} from 'immutable';
 
 const STYLE_BOX = {
-  width: "13.75em",
-  height: "13.75em",
-  padding: "0.625em",
-  background: "#f7f7f9",
-  border: "1px solid #e1e1e8",
-  margin: "0.3em",
-  cursor: "pointer",
-  position: "relative",
-  boxShadow: "0 1px 6px 0 rgba(0, 0, 0, 0.11), 0 1px 4px 0 rgba(0, 0, 0, 0.11)",
-  borderRadius: "2px"
+  width: '14em',
+  height: '14em',
+  padding: '0.625em',
+  background: '#f7f7f9',
+  border: '1px solid #e1e1e8',
+  margin: '0.3em',
+  cursor: 'pointer',
+  position: 'relative',
+  boxShadow: '0 1px 6px 0 rgba(0, 0, 0, 0.11), 0 1px 4px 0 rgba(0, 0, 0, 0.11)',
+  borderRadius: '2px'
 };
 
 const STYLE_BOX_HOVER = {
   ...STYLE_BOX,
-  background: "#1ca6fc"
+  background: '#1ca6fc'
+};
+
+const STYLE_TITLE = {
+  width:'100%',
+  textAlign:'center',
+  display:'block',
+  marginBottom:'.5em',
+  textTransform: 'capitalize'
 };
 
 const STYLE_IMAGE = {
-  background: "#222",
-  marginBottom: "5px",
-  border: "solid 1px #e6e6e6",
+  background: '#222',
+  marginBottom: '5px',
+  border: 'solid 1px #e6e6e6',
   width: '100%',
   height: '8em',
   backgroundSize: 'contain',
@@ -36,37 +43,41 @@ const STYLE_IMAGE = {
 
 const STYLE_PLUS_HOVER = {
   marginTop:'1.5em',
-  color: "#1ca6fc",
-  fontSize: "2em",
-  opacity: "0.7",
+  color: '#1ca6fc',
+  fontSize: '2em',
+  opacity: '0.7',
   width: '100%'
 };
 
 const STYLE_DESCRIPTION = {
-  fontSize: "0.75em",
-};
-
-const STYLE_ICON = {
-  position: "absolute",
-  bottom: "5px",
-  right: "10px"
+  display: 'block',
+  display: '-webkit-box',
+  height: '2em',
+  margin: '0 auto',
+  fontSize: '0.75em',
+  fontStyle:'italic',
+  lineHeight: '1em',
+  WebkitLineClamp: '2',
+  WebkitBoxOrient: 'vertical',
+  overflow: 'hidden',
+  textOverflow: 'ellipsis',
 };
 
 const STYLE_TAGS = {
-  listStyle: "none",
-  margin: "0px",
-  padding: "0px",
-  fontSize: "11px",
-  marginBottom: "3px"
+  listStyle: 'none',
+  margin: '0px',
+  padding: '0px',
+  fontSize: '11px',
+  marginBottom: '3px'
 };
 
 const STYLE_TAG = {
-  display: "inline-block",
-  background: "#337ab7",
-  color: "#fff",
-  padding: "1px 4px",
-  marginRight: "3px",
-  borderRadius: "3px"
+  display: 'inline-block',
+  background: '#337ab7',
+  color: '#fff',
+  padding: '1px 4px',
+  marginRight: '3px',
+  borderRadius: '3px'
 };
 
 export default class CatalogItem extends Component {
@@ -78,17 +89,16 @@ export default class CatalogItem extends Component {
 
   select() {
     let element = this.props.element;
-    let {linesActions, holesActions, itemsActions} = this.context;
 
     switch (element.prototype) {
       case 'lines':
-        linesActions.selectToolDrawingLine(element.name);
+        this.context.linesActions.selectToolDrawingLine(element.name);
         break;
       case 'items':
-        itemsActions.selectToolDrawingItem(element.name);
+        this.context.itemsActions.selectToolDrawingItem(element.name);
         break;
       case 'holes':
-        holesActions.selectToolDrawingHole(element.name);
+        this.context.holesActions.selectToolDrawingHole(element.name);
         break;
     }
   }
@@ -98,19 +108,20 @@ export default class CatalogItem extends Component {
     let hover = this.state.hover;
 
     return (
-      <div style={hover ? STYLE_BOX_HOVER : STYLE_BOX}
-           onClick={e => this.select()}
-           onMouseEnter={e => this.setState({hover: true})}
-           onMouseLeave={e => this.setState({hover: false})}>
+      <div
+        style={hover ? STYLE_BOX_HOVER : STYLE_BOX}
+        onClick={e => this.select()}
+        onMouseEnter={e => this.setState({hover: true})}
+        onMouseLeave={e => this.setState({hover: false})}
+      >
+        <b style={STYLE_TITLE}>{element.info.title}</b>
         <div style={{ ...STYLE_IMAGE, backgroundImage: 'url(' + element.info.image + ')'}}>
           { hover ? <IconAdd style={STYLE_PLUS_HOVER} /> : null }
         </div>
-        <b>{element.info.title}</b>
         <ul style={STYLE_TAGS}>
-          {new Seq(element.info.tag).map((tag, index) => <li style={STYLE_TAG} key={index}>{tag}</li>)}
+          {element.info.tag.map((tag, index) => <li style={STYLE_TAG} key={index}>{tag}</li>)}
         </ul>
         <div style={STYLE_DESCRIPTION}>{element.info.description}</div>
-        <IconAdd style={STYLE_ICON}/>
       </div>
     );
   }
