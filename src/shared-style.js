2d317f7466d9fa4a9dcdcb3e883583f9c4d39560
Alessio Carrafa <alessio.carrafa@hotmail.it>
2017-08-04 17:27:40 +0200

    added shared styles

---
+++ src/shared-style.js
@@ -0,0 +1,19 @@
+// COLORS
+export const COLORS = {
+  white: '#FFF',
+  black: '#000'
+};
+
+export const PRIMARY_COLOR = {
+  main: '#28292D',
+  alt: '#2E2F33',
+  icon: '#C2C2C2',
+  text_main: COLORS.white,
+  text_alt: '#EBEBEB'
+};
+
+export const SECONDARY_COLOR = {
+  main: '#1CA6FC',
+  alt: '#005FAF',
+  icon: '#1CA6FC'
+};
