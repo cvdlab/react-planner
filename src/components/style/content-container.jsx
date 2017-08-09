7849fdeb1f69b4d8ccb180fa36f1f4c776b38f20
Alessio Carrafa <alessio.carrafa@hotmail.it>
2017-08-09 15:54:41 +0200

    added style property for content-container and content-title

--- src/components/style/content-container.jsx
+++ src/components/style/content-container.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const STYLE = {
-  padding: "0 20px",
-  overflowY: "auto"
+  padding: '0 20px',
+  overflowY: 'auto'
 };
 
-export default function ContentContainer({children, width, height}) {
-  return <div style={{width, height, ...STYLE}} onWheel={event => event.stopPropagation()}>{children}</div>
+export default function ContentContainer({children, width, height, style = {}}) {
+  return <div style={{width, height, ...STYLE, ...style}} onWheel={event => event.stopPropagation()}>{children}</div>
 }
 
 ContentContainer.propsType = {
   width: PropTypes.number.isRequired,
   height: PropTypes.number.isRequired,
+  style: PropTypes.object
 };
-
--- src/components/style/content-title.jsx
+++ src/components/style/content-title.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import * as SharedStyle from '../../shared-style';
 
 const STYLE = {
   color: SharedStyle.PRIMARY_COLOR.alt,
-  fontWeight: "300",
+  fontWeight: 300,
 };
 
-export default function ContentTitle({children, ...rest}) {
-  return <h1 style={STYLE} {...rest}>{children}</h1>
+export default function ContentTitle({children, style = {}, ...rest}) {
+  return <h1 style={{...STYLE, ...style}} {...rest}>{children}</h1>
 }
+
+ContentTitle.propsType = {
+  style: PropTypes.object
+};
