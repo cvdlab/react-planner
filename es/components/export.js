48f7328feb59173fbd672b3e27eee7f713322dcb
Alessio Carrafa <alessio.carrafa@hotmail.it>
2017-09-12 19:01:16 +0200

    1.6.5

--- es/components/export.js
+++ es/components/export.js
@@ -3,13 +3,15 @@ import Content from './content';
 import SidebarComponents from './sidebar/export';
 import FooterBarComponents from './footerbar/export';
 import Viewer2DComponents from './viewer2d/export';
+import StyleComponents from './style/export';
 
-export { ToolbarComponents, Content, SidebarComponents, FooterBarComponents, Viewer2DComponents };
+export { ToolbarComponents, Content, SidebarComponents, FooterBarComponents, Viewer2DComponents, StyleComponents };
 
 export default {
   ToolbarComponents: ToolbarComponents,
   Content: Content,
   SidebarComponents: SidebarComponents,
   FooterBarComponents: FooterBarComponents,
-  Viewer2DComponents: Viewer2DComponents
+  Viewer2DComponents: Viewer2DComponents,
+  StyleComponents: StyleComponents
 };
