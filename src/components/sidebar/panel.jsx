e36ecb54b728f7571d2356cf049af4b76f55484e
Alessio Carrafa <alessio.carrafa@hotmail.it>
2017-09-15 17:44:57 +0200

    Added ability to add jsx for Panel component header

--- src/components/sidebar/panel.jsx
+++ src/components/sidebar/panel.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import * as SharedStyle from '../../shared-style';
 
-const STYLE = {borderTop: '1px solid #222222', borderBottom: '1px solid #48494E'};
+const STYLE = {borderTop: '1px solid #222', borderBottom: '1px solid #48494E'};
 const STYLE_TITLE = {
   fontSize: '11px',
   color: SharedStyle.PRIMARY_COLOR.text_alt,
-  padding: '5px 0 8px 15px',
+  padding: '5px 15px 8px 15px',
   backgroundColor: SharedStyle.PRIMARY_COLOR.alt,
   textShadow: '-1px -1px 2px rgba(0, 0, 0, 1)',
   boxShadow: 'inset 0px -3px 19px 0px rgba(0,0,0,0.5)',
@@ -14,17 +14,18 @@ const STYLE_TITLE = {
 const STYLE_CONTENT = {
   fontSize: '11px',
   color: SharedStyle.PRIMARY_COLOR.text_alt,
-  border: '1px solid #222222',
+  border: '1px solid #222',
   padding: '0px',
   backgroundColor: SharedStyle.PRIMARY_COLOR.alt,
   textShadow: '-1px -1px 2px rgba(0, 0, 0, 1)'
 };
 
-export default function Panel({name, children}) {
+export default function Panel({name, headComponents, children}) {
   return (
     <div style={STYLE}>
       <h3 style={STYLE_TITLE}>
         {name}
+        {headComponents}
       </h3>
 
       <div style={STYLE_CONTENT}>
