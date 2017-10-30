4aa18f242f738eeed6068e28e8f3d7ccb5dcdae8
Alessio Carrafa <alessio.carrafa@hotmail.it>
2017-10-30 15:22:24 +0100

    toolbar component is now stateful allowing performance improve

--- src/components/toolbar/toolbar.jsx
+++ src/components/toolbar/toolbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import IconNewFile from 'react-icons/lib/fa/file-o';
 import IconPointer from 'react-icons/lib/fa/mouse-pointer';
@@ -96,100 +96,114 @@ const mapButtonsCb = (el, ind) => {
   );
 };
 
-export default function Toolbar(
-  { state, width, height, toolbarButtons, allowProjectFileSupport},
-  { projectActions, viewer2DActions, viewer3DActions, linesActions, holesActions, itemsActions, translator}
-){
+export default class Toolbar extends Component {
 
-  let mode = state.get('mode');
-  let mode3DCondition = ![MODE_3D_FIRST_PERSON, MODE_3D_VIEW].includes(mode);
+  constructor(props, context) {
+    super(props, context);
+    this.state = {};
+  }
 
-  let sorter = [
-    {
-      index: 0, condition: allowProjectFileSupport, dom: <ToolbarButton
-      active={false}
-      tooltip={translator.t('New project')}
-      onClick={event => confirm(translator.t('Would you want to start a new Project?')) ? projectActions.newProject() : null}>
-      <IconNewFile />
-    </ToolbarButton>
-    },
-    {
-      index: 1, condition: allowProjectFileSupport,
-      dom: <ToolbarSaveButton state={state}/>
-    },
-    {
-      index: 2, condition: allowProjectFileSupport,
-      dom: <ToolbarLoadButton state={state}/>
-    },
-    {
-      index: 3, condition: true,
-      dom: <ToolbarButton
-        active={[MODE_VIEWING_CATALOG].includes(mode)}
-        tooltip={translator.t('Open catalog')}
-        onClick={event => projectActions.openCatalog()}>
-        <IconCatalog />
+  shouldComponentUpdate(nextProps, nextState) {
+    return this.props.state.mode !== nextProps.state.mode;
+  }
+
+  render() {
+
+    let {
+      props: { state, width, height, toolbarButtons, allowProjectFileSupport},
+      context: { projectActions, viewer2DActions, viewer3DActions, linesActions, holesActions, itemsActions, translator}
+    } = this;
+
+    let mode = state.get('mode');
+    let mode3DCondition = ![MODE_3D_FIRST_PERSON, MODE_3D_VIEW].includes(mode);
+
+    let sorter = [
+      {
+        index: 0, condition: allowProjectFileSupport, dom: <ToolbarButton
+        active={false}
+        tooltip={translator.t('New project')}
+        onClick={event => confirm(translator.t('Would you want to start a new Project?')) ? projectActions.newProject() : null}>
+        <IconNewFile />
       </ToolbarButton>
-    },
-    {
-      index: 4, condition: true, dom: <ToolbarButton
-      active={[MODE_3D_VIEW].includes(mode)}
-      tooltip={translator.t('3D View')}
-      onClick={event => viewer3DActions.selectTool3DView()}>
-      <Icon3D />
-    </ToolbarButton>
-    },
-    {
-      index: 5, condition: true, dom: <ToolbarButton
-      active={[MODE_IDLE].includes(mode)}
-      tooltip={translator.t('2D View')}
-      onClick={event => projectActions.rollback()}>
-      {[MODE_3D_FIRST_PERSON, MODE_3D_VIEW].includes(mode) ? <Icon2D/> : <IconPointer/>}
-    </ToolbarButton>
-    },
-    {
-      index: 6, condition: true, dom: <ToolbarButton
-      active={[MODE_3D_FIRST_PERSON].includes(mode)}
-      tooltip={translator.t('3D First Person')}
-      onClick={event => viewer3DActions.selectTool3DFirstPerson()}>
-      <Icon3DFirstPerson />
-    </ToolbarButton>
-    },
-    {
-      index: 7, condition: true, dom: <ToolbarButton
-      active={false}
-      tooltip={translator.t('Undo (CTRL-Z)')}
-      onClick={event => projectActions.undo()}>
-      <IconUndo />
-    </ToolbarButton>
-    },
-    {
-      index: 8, condition: true, dom: <ToolbarButton
-      active={[MODE_CONFIGURING_PROJECT].includes(mode)}
-      tooltip={translator.t('Configure project')}
-      onClick={event => projectActions.openProjectConfigurator()}>
-      <IconConfigure />
-    </ToolbarButton>
-    }
-  ];
+      },
+      {
+        index: 1, condition: allowProjectFileSupport,
+        dom: <ToolbarSaveButton state={state}/>
+      },
+      {
+        index: 2, condition: allowProjectFileSupport,
+        dom: <ToolbarLoadButton state={state}/>
+      },
+      {
+        index: 3, condition: true,
+        dom: <ToolbarButton
+          active={[MODE_VIEWING_CATALOG].includes(mode)}
+          tooltip={translator.t('Open catalog')}
+          onClick={event => projectActions.openCatalog()}>
+          <IconCatalog />
+        </ToolbarButton>
+      },
+      {
+        index: 4, condition: true, dom: <ToolbarButton
+        active={[MODE_3D_VIEW].includes(mode)}
+        tooltip={translator.t('3D View')}
+        onClick={event => viewer3DActions.selectTool3DView()}>
+        <Icon3D />
+      </ToolbarButton>
+      },
+      {
+        index: 5, condition: true, dom: <ToolbarButton
+        active={[MODE_IDLE].includes(mode)}
+        tooltip={translator.t('2D View')}
+        onClick={event => projectActions.rollback()}>
+        {[MODE_3D_FIRST_PERSON, MODE_3D_VIEW].includes(mode) ? <Icon2D/> : <IconPointer/>}
+      </ToolbarButton>
+      },
+      {
+        index: 6, condition: true, dom: <ToolbarButton
+        active={[MODE_3D_FIRST_PERSON].includes(mode)}
+        tooltip={translator.t('3D First Person')}
+        onClick={event => viewer3DActions.selectTool3DFirstPerson()}>
+        <Icon3DFirstPerson />
+      </ToolbarButton>
+      },
+      {
+        index: 7, condition: true, dom: <ToolbarButton
+        active={false}
+        tooltip={translator.t('Undo (CTRL-Z)')}
+        onClick={event => projectActions.undo()}>
+        <IconUndo />
+      </ToolbarButton>
+      },
+      {
+        index: 8, condition: true, dom: <ToolbarButton
+        active={[MODE_CONFIGURING_PROJECT].includes(mode)}
+        tooltip={translator.t('Configure project')}
+        onClick={event => projectActions.openProjectConfigurator()}>
+        <IconConfigure />
+      </ToolbarButton>
+      }
+    ];
 
-  sorter = sorter.concat(toolbarButtons.map((Component, key) => {
-    return Component.prototype ? //if is a react component
-    {
-      condition: true,
-      dom: React.createElement(Component, { mode, state, key })
-    }:
-    {                           //else is a sortable toolbar button
-      index: Component.index,
-      condition: Component.condition,
-      dom: React.createElement( Component.dom, { mode, state, key })
-    };
-  }));
+    sorter = sorter.concat(toolbarButtons.map((Component, key) => {
+      return Component.prototype ? //if is a react component
+      {
+        condition: true,
+        dom: React.createElement(Component, { mode, state, key })
+      }:
+      {                           //else is a sortable toolbar button
+        index: Component.index,
+        condition: Component.condition,
+        dom: React.createElement( Component.dom, { mode, state, key })
+      };
+    }));
 
-  return (
-    <aside style={{...ASIDE_STYLE, maxWidth: width, maxHeight: height}} className='toolbar'>
-      {sorter.sort(sortButtonsCb).map(mapButtonsCb)}
-    </aside>
-  )
+    return (
+      <aside style={{...ASIDE_STYLE, maxWidth: width, maxHeight: height}} className='toolbar'>
+        {sorter.sort(sortButtonsCb).map(mapButtonsCb)}
+      </aside>
+    )
+  }
 }
 
 Toolbar.propTypes = {
