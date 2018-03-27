f27b81acaee1caefa6cf4b780ee2db2e5f1b9f7b
Alessio Carrafa <alessio.carrafa@hotmail.it>
2018-03-27 18:24:50 +0200

    added sidebar's components sort functionality

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -5,6 +5,7 @@ import PanelLayers from './panel-layers';
 import PanelGuides from './panel-guides';
 import PanelLayerElements from './panel-layer-elements';
 import * as SharedStyle from '../../shared-style';
+import If from '../../utils/react-if';
 
 const STYLE = {
   backgroundColor: SharedStyle.PRIMARY_COLOR.main,
@@ -14,20 +15,50 @@ const STYLE = {
   paddingBottom: '20px'
 };
 
-export default function Sidebar({state, width, height, sidebarComponents}) {
+const sortButtonsCb = (a, b) => {
+  if (a.index === undefined || a.index === null) {
+    a.index = Number.MAX_SAFE_INTEGER;
+  }
+
+  if (b.index === undefined || b.index === null) {
+    b.index = Number.MAX_SAFE_INTEGER;
+  }
+
+  return a.index - b.index;
+};
+
+const mapButtonsCb = (el, ind) => <If key={ind} condition={el.condition} style={{ position: 'relative' }}>{el.dom}</If>;
+
+export default function Sidebar({ state, width, height, sidebarComponents }) {
+
+  let sorter = [
+    { index: 0, condition: true, dom: <PanelLayers state={state} /> },
+    { index: 1, condition: true, dom: <PanelLayerElements mode={state.mode} layers={state.scene.layers} selectedLayer={state.scene.selectedLayer} /> },
+    { index: 2, condition: true, dom: <PanelElementEditor state={state} /> },
+    //{ index: 999999, condition: true, dom: <PanelGuides state={state}/> },
+  ];
+
+  sorter = sorter.concat(sidebarComponents.map((Component, key) => {
+    return Component.prototype ? //if is a react component
+      {
+        condition: true,
+        dom: React.createElement(Component, { state, key })
+      } :
+      {                           //else is a sortable toolbar button
+        index: Component.index,
+        condition: Component.condition,
+        dom: React.createElement(Component.dom, { state, key })
+      };
+  }));
 
   return (
     <aside
-      style={{width, height, ...STYLE}}
+      style={{ width, height, ...STYLE }}
       onKeyDown={event => event.stopPropagation()}
       onKeyUp={event => event.stopPropagation()}
       className="sidebar"
     >
-      <div className="layers"><PanelLayers state={state}/></div>
-      <div className="layer-elements"><PanelLayerElements mode={state.mode} layers={state.scene.layers} selectedLayer={state.scene.selectedLayer}/></div>
-      <div className="properties"><PanelElementEditor state={state}/></div>
-      {/*<div className="guides"><PanelGuides state={state}/></div>*/}
-      {sidebarComponents.map((Component, index) => <Component state={state} key={index}/>)}
+      {sorter.sort(sortButtonsCb).map(mapButtonsCb)}
     </aside>
   );
 }
