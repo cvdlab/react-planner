198df715064633d3371b011e762625ed4a2d38a1
Alessio Carrafa <alessio.carrafa@hotmail.it>
2018-03-29 10:03:05 +0200

    fixed wall texture load in demo

--- demo/src/catalog/lines/wall/planner-element.jsx
+++ demo/src/catalog/lines/wall/planner-element.jsx
@@ -41,25 +41,5 @@ const textures = {
   },
 };
 
-const wall = ElementsFactories.WallFactory('wall', info, textures);
-
-let textureValues = {
-  'none': 'None'
-};
-
-wall.properties.textureA = {
-  label: 'Covering A',
-  type: 'enum',
-  defaultValue: 'bricks',
-  values: textureValues
-};
-
-wall.properties.textureB = {
-  label: 'Covering B',
-  type: 'enum',
-  defaultValue: 'bricks',
-  values: textureValues
-};
-
-export default wall;
+export default ElementsFactories.WallFactory('wall', info, textures);
 
