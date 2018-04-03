ae3d10eda58361f262d8c9e0eb06725ffbce3f1b
Alessio Carrafa <alessio.carrafa@hotmail.it>
2018-04-03 17:35:36 +0200

    added show/hide button to Panel

--- src/components/sidebar/panel.jsx
+++ src/components/sidebar/panel.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import * as SharedStyle from '../../shared-style';
+import { FaAngleDown, FaAngleUp } from 'react-icons/lib/fa';
 
-const STYLE = {borderTop: '1px solid #222', borderBottom: '1px solid #48494E'};
+const STYLE = {
+  borderTop: '1px solid #222',
+  borderBottom: '1px solid #48494E',
+  userSelect: 'none'
+};
 const STYLE_TITLE = {
   fontSize: '11px',
   color: SharedStyle.PRIMARY_COLOR.text_alt,
@@ -9,7 +15,8 @@ const STYLE_TITLE = {
   backgroundColor: SharedStyle.PRIMARY_COLOR.alt,
   textShadow: '-1px -1px 2px rgba(0, 0, 0, 1)',
   boxShadow: 'inset 0px -3px 19px 0px rgba(0,0,0,0.5)',
-  margin: '0px'
+  margin: '0px',
+  cursor: 'pointer'
 };
 const STYLE_CONTENT = {
   fontSize: '11px',
@@ -19,18 +26,61 @@ const STYLE_CONTENT = {
   backgroundColor: SharedStyle.PRIMARY_COLOR.alt,
   textShadow: '-1px -1px 2px rgba(0, 0, 0, 1)'
 };
+const STYLE_ARROW = {
+  float: 'right'
+};
 
-export default function Panel({name, headComponents, children}) {
-  return (
-    <div style={STYLE}>
-      <h3 style={STYLE_TITLE}>
-        {name}
-        {headComponents}
-      </h3>
+export default class Panel extends Component {
 
-      <div style={STYLE_CONTENT}>
-        {children}
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      opened: props.hasOwnProperty('opened') ? props.opened : false,
+      hover: false
+    };
+  }
+
+  toggleOpen() {
+    this.setState({opened: !this.state.opened});
+  }
+
+  toggleHover() {
+    this.setState({hover: !this.state.hover});
+  }
+
+  render() {
+
+    let { name, headComponents, children } = this.props;
+    let { opened, hover } = this.state;
+
+    return (
+      <div style={STYLE}>
+        <h3
+          style={{...STYLE_TITLE, color: hover ? SharedStyle.SECONDARY_COLOR.main : SharedStyle.PRIMARY_COLOR.text_alt}}
+          onMouseEnter={() => this.toggleHover()}
+          onMouseLeave={() => this.toggleHover()}
+          onClick={() => this.toggleOpen()}
+        >
+          {name}
+          {headComponents}
+          {
+            opened ?
+            <FaAngleUp style={STYLE_ARROW} /> :
+            <FaAngleDown style={STYLE_ARROW} />
+          }
+        </h3>
+
+        <div style={{...STYLE_CONTENT, display: opened ? 'block' : 'none'}}>
+          {children}
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
+
+Panel.propTypes = {
+  name: PropTypes.string.isRequired,
+  headComponents: PropTypes.array,
+  opened: PropTypes.bool
+};
