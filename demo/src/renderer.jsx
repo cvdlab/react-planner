535b7339c1ad2e384d373a806d49e8dbcd9251e1
Alessio Carrafa <alessio.carrafa@hotmail.it>
2018-04-11 17:40:07 +0200

    added devToolsExtension configuration if in development mode, disable devToolsExtension if in production mode

--- demo/src/renderer.jsx
+++ demo/src/renderer.jsx
@@ -16,12 +16,10 @@ import {
   Plugins as PlannerPlugins,
 } from 'react-planner'; //react-planner
 
-
 //define state
 let AppState = Map({
   'react-planner': new PlannerModels.State()
 });
-
 
 //define reducer
 let reducer = (state, action) => {
@@ -30,9 +28,38 @@ let reducer = (state, action) => {
   return state;
 };
 
+let blackList = isProduction === true ? [] : [
+  'UPDATE_MOUSE_COORDS',
+  'UPDATE_ZOOM_SCALE',
+  'UPDATE_2D_CAMERA'
+];
+
+if( !isProduction ) console.info('Environment is in development and these actions will be blacklisted', blackList);
 
 //init store
-let store = createStore(reducer, null, window.devToolsExtension ? window.devToolsExtension() : f => f);
+let store = createStore(
+  reducer,
+  null,
+  !isProduction && window.devToolsExtension ?
+    window.devToolsExtension({
+      features: {
+        pause   : true,     // start/pause recording of dispatched actions
+        lock    : true,     // lock/unlock dispatching actions and side effects
+        persist : true,     // persist states on page reloading
+        export  : true,     // export history of actions in a file
+        import  : 'custom', // import history of actions from a file
+        jump    : true,     // jump back and forth (time travelling)
+        skip    : true,     // skip (cancel) actions
+        reorder : true,     // drag and drop actions in the history list
+        dispatch: true,     // dispatch custom actions or action creators
+        test    : true      // generate tests for the selected actions
+      },
+      actionsBlacklist: blackList,
+      maxAge: 999999
+    }) :
+    f => f
+);
+
 let plugins = [
   PlannerPlugins.Keyboard(),
   PlannerPlugins.Autosave('react-planner_v0'),
@@ -49,14 +76,18 @@ ReactDOM.render(
     <Provider store={store}>
       <ContainerDimensions>
         {({width, height}) =>
-          <ReactPlanner catalog={MyCatalog} width={width} height={height} plugins={plugins}
-                        toolbarButtons={toolbarButtons} stateExtractor={state => state.get('react-planner')}
+          <ReactPlanner
+            catalog={MyCatalog}
+            width={width}
+            height={height}
+            plugins={plugins}
+            toolbarButtons={toolbarButtons}
+            stateExtractor={state => state.get('react-planner')}
           />
         }
       </ContainerDimensions>
     </Provider>
   ),
-
   document.getElementById('app')
 );
 
