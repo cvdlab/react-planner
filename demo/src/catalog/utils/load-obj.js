20637e594666b5ab9bcc1f58369f2d8e2d9c6a2a
Danilo Salvati <salvati.danilo@gmail.com>
2018-04-20 16:58:45 +0200

    Code cleaning

--- demo/src/catalog/utils/load-obj.js
+++ demo/src/catalog/utils/load-obj.js
@@ -1,23 +1,5 @@
-import Three from 'three';
 import MTLLoader from './mtl-loader';
 import OBJLoader from './obj-loader';
-
-// export function loadObj(objPath, objFile, isSelected) {
-//   let objLoader = new OBJLoader();
-//   objLoader.setPath(objPath);
-//   return new Promise((resolve, reject) => {
-//     objLoader.load(objFile, object => {
-//
-//       if (isSelected) {
-//         let box = new Three.BoxHelper(object, 0x99c3fb);
-//         box.material.linewidth = 2;
-//         box.material.depthTest = false;
-//         object.add(box);
-//       }
-//       resolve(object);
-//     });
-//   });
-// }
 
 export function loadObjWithMaterial(mtlFile, objFile, imgPath) {
   let mtlLoader = new MTLLoader();
