492c5a6c63bb952fc558c3759b1cb0178a8872b2
Alessio Carrafa <alessio.carrafa@hotmail.it>
2018-05-07 12:21:14 +0200

    area's reduces now use new class approach

--- src/reducers/areas-reducer.js
+++ src/reducers/areas-reducer.js
@@ -1,40 +1,12 @@
-import {
-  select,
-  unselectAll,
-} from '../utils/layer-operations';
 
-import {history} from '../utils/export';
-
+import { Area } from '../class/export';
 import {SELECT_AREA} from '../constants';
 
 export default function (state, action) {
   switch (action.type) {
     case SELECT_AREA:
-      return selectArea(state, action.layerID, action.areaID);
-
+      return Area.select( state, action.layerID, action.areaID ).updatedState;
     default:
       return state;
   }
-}
-
-
-function selectArea(state, layerID, areaID) {
-  let scene = state.scene;
-
-  scene = scene.merge({
-    layers: state.alterate ? scene.layers : scene.layers.map(unselectAll),
-    selectedLayer: layerID
-  });
-
-  scene = scene.updateIn(['layers', layerID], layer => layer.withMutations(layer => {
-      let area = layer.getIn(['areas', areaID]);
-      select(layer, 'areas', areaID);
-      area.vertices.forEach(vertexID => select(layer, 'vertices', vertexID));
-    })
-  );
-
-  return state.merge({
-    scene,
-    sceneHistory: history.historyPush( state.sceneHistory, scene )
-  });
 }
