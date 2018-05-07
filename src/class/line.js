c4aacfcf3c5c9699fc89ea31e78dbb7c9d80fed0
Alessio Carrafa <alessio.carrafa@hotmail.it>
2018-05-07 12:18:51 +0200

    moved functions from line's reducer to line's class

--- src/class/line.js
+++ src/class/line.js
@@ -1,4 +1,4 @@
-import {List} from 'immutable';
+import {Map, List} from 'immutable';
 import {
   Group,
   Layer,
@@ -10,8 +10,16 @@ import {
   history,
   IDBroker,
   NameGenerator,
-  GeometryUtils
+  GeometryUtils,
+  SnapUtils,
+  SnapSceneUtils,
 } from '../utils/export';
+import {
+  MODE_IDLE,
+  MODE_WAITING_DRAWING_LINE,
+  MODE_DRAWING_LINE,
+  MODE_DRAGGING_LINE
+} from '../constants';
 
 class Line{
 
@@ -230,6 +238,292 @@ class Line{
     state = state.setIn(['scene', 'layers', layerID, 'lines', lineID], state.getIn(['scene', 'layers', layerID, 'lines', lineID]) );
 
     return { updatedState: state, line: state.getIn(['scene', 'layers', layerID, 'lines', lineID]), vertex };
+  }
+
+  static selectToolDrawingLine(state, sceneComponentType) {
+    state = state.merge({
+      mode: MODE_WAITING_DRAWING_LINE,
+      drawingSupport: new Map({
+        type: sceneComponentType
+      })
+    });
+
+    return { updatedState: state };
+  }
+
+  static beginDrawingLine(state, layerID, x, y) {
+    let snapElements = SnapSceneUtils.sceneSnapElements(state.scene, new List(), state.snapMask);
+    let snap = null;
+
+    if (state.snapMask && !state.snapMask.isEmpty()) {
+      snap = SnapUtils.nearestSnap(snapElements, x, y, state.snapMask);
+      if (snap) ({x, y} = snap.point);
+
+      snapElements = snapElements.withMutations(snapElements => {
+        let a, b, c;
+        ({a, b, c} = GeometryUtils.horizontalLine(y));
+        SnapUtils.addLineSnap(snapElements, a, b, c, 10, 3, null);
+        ({a, b, c} = GeometryUtils.verticalLine(x));
+        SnapUtils.addLineSnap(snapElements, a, b, c, 10, 3, null);
+      });
+    }
+
+    let drawingSupport = state.get('drawingSupport').set('layerID', layerID);
+
+    state = Layer.unselectAll( state, layerID ).updatedState;
+
+    let { updatedState: stateL, line } = Line.create( state, layerID, drawingSupport.get('type'), x, y, x, y );
+    state = Line.select( stateL, layerID, line.id ).updatedState;
+
+    state = state.merge({
+      mode: MODE_DRAWING_LINE,
+      snapElements,
+      activeSnapElement: snap ? snap.snap : null,
+      drawingSupport
+    });
+
+    return { updatedState: state };
+  }
+
+  static updateDrawingLine(state, x, y) {
+    let snap = null;
+    if (state.snapMask && !state.snapMask.isEmpty()) {
+      snap = SnapUtils.nearestSnap(state.snapElements, x, y, state.snapMask);
+      if (snap) ({x, y} = snap.point);
+    }
+
+    let layerID = state.getIn(['drawingSupport', 'layerID']);
+
+    let layer = state.getIn(['scene', 'layers', layerID]);
+
+    let lineID = layer.getIn(['selected', 'lines']).first();
+    let { updatedState: stateLV, vertex } = Line.replaceVertex( state, layerID, lineID, 1, x, y );
+    state = stateLV;
+
+    state = Layer.select( state, layerID, 'vertices', vertex.id).updatedState;
+
+    state = state.merge({ activeSnapElement: snap ? snap.snap : null });
+
+    return { updatedState: state };
+  }
+
+  static endDrawingLine(state, x, y) {
+
+    if (state.snapMask && !state.snapMask.isEmpty()) {
+      let snap = SnapUtils.nearestSnap(state.snapElements, x, y, state.snapMask);
+      if (snap) ({x, y} = snap.point);
+    }
+
+    let layerID = state.getIn(['drawingSupport', 'layerID']);
+    let layer = state.getIn(['scene','layers', layerID]);
+
+    let lineID = state.getIn(['scene', 'layers', layerID, 'selected', 'lines']).first();
+    let line = state.getIn(['scene', 'layers', layerID, 'lines', lineID]);
+
+    let v0 = layer.vertices.get(line.vertices.get(0));
+
+    state = Line.remove( state, layerID, lineID ).updatedState;
+    state = Line.createAvoidingIntersections( state, layerID, line.type, v0.x, v0.y, x, y ).updatedState;
+    state = Layer.detectAndUpdateAreas( state, layerID ).updatedState;
+
+    state = state.merge({
+      mode: MODE_WAITING_DRAWING_LINE,
+      snapElements: new List(),
+      activeSnapElement: null,
+      sceneHistory: history.historyPush( state.sceneHistory, state.scene )
+    });
+
+    return { updatedState: state };
+  }
+
+  static beginDraggingLine(state, layerID, lineID, x, y) {
+
+    let snapElements = SnapSceneUtils.sceneSnapElements(state.scene, new List(), state.snapMask);
+
+    let layer = state.scene.layers.get(layerID);
+    let line = layer.lines.get(lineID);
+
+    let vertex0 = layer.vertices.get(line.vertices.get(0));
+    let vertex1 = layer.vertices.get(line.vertices.get(1));
+
+    state = state.merge({
+      mode: MODE_DRAGGING_LINE,
+      snapElements,
+      draggingSupport: Map({
+        layerID, lineID,
+        startPointX: x,
+        startPointY: y,
+        startVertex0X: vertex0.x,
+        startVertex0Y: vertex0.y,
+        startVertex1X: vertex1.x,
+        startVertex1Y: vertex1.y,
+      })
+    });
+
+    return { updatedState: state };
+  }
+
+  static updateDraggingLine(state, x, y) {
+
+    let draggingSupport = state.draggingSupport;
+    let snapElements = state.snapElements;
+
+    let layerID = draggingSupport.get('layerID');
+    let lineID = draggingSupport.get('lineID');
+    let diffX = x - draggingSupport.get('startPointX');
+    let diffY = y - draggingSupport.get('startPointY');
+    let newVertex0X = draggingSupport.get('startVertex0X') + diffX;
+    let newVertex0Y = draggingSupport.get('startVertex0Y') + diffY;
+    let newVertex1X = draggingSupport.get('startVertex1X') + diffX;
+    let newVertex1Y = draggingSupport.get('startVertex1Y') + diffY;
+
+
+    let activeSnapElement = null;
+    let curSnap0 = null, curSnap1 = null;
+    if (state.snapMask && !state.snapMask.isEmpty()) {
+      curSnap0 = SnapUtils.nearestSnap(snapElements, newVertex0X, newVertex0Y, state.snapMask);
+      curSnap1 = SnapUtils.nearestSnap(snapElements, newVertex1X, newVertex1Y, state.snapMask);
+    }
+
+    let deltaX = 0, deltaY = 0;
+    if (curSnap0 && curSnap1) {
+      if (curSnap0.point.distance < curSnap1.point.distance) {
+        deltaX = curSnap0.point.x - newVertex0X;
+        deltaY = curSnap0.point.y - newVertex0Y;
+        activeSnapElement = curSnap0.snap;
+      } else {
+        deltaX = curSnap1.point.x - newVertex1X;
+        deltaY = curSnap1.point.y - newVertex1Y;
+        activeSnapElement = curSnap1.snap;
+      }
+    } else {
+      if (curSnap0) {
+        deltaX = curSnap0.point.x - newVertex0X;
+        deltaY = curSnap0.point.y - newVertex0Y;
+        activeSnapElement = curSnap0.snap;
+      }
+      if (curSnap1) {
+        deltaX = curSnap1.point.x - newVertex1X;
+        deltaY = curSnap1.point.y - newVertex1Y;
+        activeSnapElement = curSnap1.snap;
+      }
+    }
+
+    newVertex0X += deltaX;
+    newVertex0Y += deltaY;
+    newVertex1X += deltaX;
+    newVertex1Y += deltaY;
+
+    state = state.merge({
+      activeSnapElement,
+      scene: state.scene.updateIn(['layers', layerID], layer => layer.withMutations(layer => {
+        let lineVertices = layer.getIn(['lines', lineID, 'vertices']);
+        layer.updateIn(['vertices', lineVertices.get(0)], vertex => vertex.merge({x: newVertex0X, y: newVertex0Y}));
+        layer.updateIn(['vertices', lineVertices.get(1)], vertex => vertex.merge({x: newVertex1X, y: newVertex1Y}));
+        return layer;
+      }))
+    });
+
+    return { updatedState: state };
+  }
+
+  static endDraggingLine(state, x, y) {
+    let {draggingSupport} = state;
+    let layerID = draggingSupport.get('layerID');
+    let layer = state.scene.layers.get(layerID);
+    let lineID = draggingSupport.get('lineID');
+    let line = layer.lines.get(lineID);
+
+    let vertex0 = layer.vertices.get(line.vertices.get(0));
+    let vertex1 = layer.vertices.get(line.vertices.get(1));
+
+    let maxV = GeometryUtils.maxVertex(vertex0, vertex1);
+    let minV = GeometryUtils.minVertex(vertex0, vertex1);
+
+    let lineLength = GeometryUtils.verticesDistance(minV,maxV);
+    let alpha = Math.atan2(maxV.y - minV.y, maxV.x - minV.x);
+
+    let holesWithOffsetPosition = [];
+    layer.lines.get(lineID).holes.forEach(holeID => {
+      let hole = layer.holes.get(holeID);
+      let pointOnLine = lineLength * hole.offset;
+
+      let offsetPosition = {
+        x: pointOnLine * Math.cos(alpha) + minV.x,
+        y: pointOnLine * Math.sin(alpha) + minV.y
+      };
+
+      holesWithOffsetPosition.push({hole, offsetPosition});
+    });
+
+    let diffX = x - draggingSupport.get('startPointX');
+    let diffY = y - draggingSupport.get('startPointY');
+    let newVertex0X = draggingSupport.get('startVertex0X') + diffX;
+    let newVertex0Y = draggingSupport.get('startVertex0Y') + diffY;
+    let newVertex1X = draggingSupport.get('startVertex1X') + diffX;
+    let newVertex1Y = draggingSupport.get('startVertex1Y') + diffY;
+
+    if (state.snapMask && !state.snapMask.isEmpty()) {
+
+      let curSnap0 = SnapUtils.nearestSnap(state.snapElements, newVertex0X, newVertex0Y, state.snapMask);
+      let curSnap1 = SnapUtils.nearestSnap(state.snapElements, newVertex1X, newVertex1Y, state.snapMask);
+
+      let deltaX = 0, deltaY = 0;
+      if (curSnap0 && curSnap1) {
+        if (curSnap0.point.distance < curSnap1.point.distance) {
+          deltaX = curSnap0.point.x - newVertex0X;
+          deltaY = curSnap0.point.y - newVertex0Y;
+        } else {
+          deltaX = curSnap1.point.x - newVertex1X;
+          deltaY = curSnap1.point.y - newVertex1Y;
+        }
+      } else {
+        if (curSnap0) {
+          deltaX = curSnap0.point.x - newVertex0X;
+          deltaY = curSnap0.point.y - newVertex0Y;
+        }
+        if (curSnap1) {
+          deltaX = curSnap1.point.x - newVertex1X;
+          deltaY = curSnap1.point.y - newVertex1Y;
+        }
+      }
+
+      newVertex0X += deltaX;
+      newVertex0Y += deltaY;
+      newVertex1X += deltaX;
+      newVertex1Y += deltaY;
+    }
+
+    state = Layer.mergeEqualsVertices( state, layerID, line.vertices.get(0) ).updatedState;
+    state = Layer.mergeEqualsVertices( state, layerID, line.vertices.get(1) ).updatedState;
+
+    state = Line.remove( state, layerID, lineID ).updatedState;
+
+    if(!GeometryUtils.samePoints({newVertex0X, newVertex0Y}, {newVertex1X, newVertex1Y})) {
+      state = Line.createAvoidingIntersections(
+        state,
+        layerID,
+        line.type,
+        newVertex0X,
+        newVertex0Y,
+        newVertex1X,
+        newVertex1Y,
+        line.properties,
+        holesWithOffsetPosition
+      ).updatedState;
+    }
+
+    state = Layer.detectAndUpdateAreas( state, layerID ).updatedState;
+
+    state = state.merge({
+      mode: MODE_IDLE,
+      draggingSupport: null,
+      activeSnapElement: null,
+      snapElements: new List(),
+      sceneHistory: history.historyPush( state.sceneHistory, state.scene )
+    });
+
+    return { updatedState: state };
   }
 
 }
