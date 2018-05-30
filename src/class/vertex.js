462d534fa79d4e72b2fc620f0055195df95d2864
Alessio Carrafa <alessio.carrafa@hotmail.it>
2018-05-30 11:45:23 +0200

    fixed endDraggingVertex on vertex without line

--- src/class/vertex.js
+++ src/class/vertex.js
@@ -130,73 +130,76 @@ class Vertex{
     let { draggingSupport } = state;
     let layerID = draggingSupport.get('layerID');
     let vertexID = draggingSupport.get('vertexID');
+    let lines = state.getIn(['scene', 'layers', layerID, 'vertices', vertexID, 'lines']);
 
-    let reduced = state.getIn(['scene', 'layers', layerID, 'vertices', vertexID, 'lines']).reduce(
-      ( reducedState, lineID ) =>
-      {
-        if( !reducedState.getIn(['scene', 'layers', layerID, 'lines', lineID])) return reducedState;
+    if( lines ) {
+      state = lines.reduce(
+        ( reducedState, lineID ) =>
+        {
+          if( !reducedState.getIn(['scene', 'layers', layerID, 'lines', lineID])) return reducedState;
 
-        let v_id0 = reducedState.getIn(['scene', 'layers', layerID, 'lines', lineID, 'vertices', 0]);
-        let v_id1 = reducedState.getIn(['scene', 'layers', layerID, 'lines', lineID, 'vertices', 1]);
-        let oldVertexID = v_id0 === vertexID ? v_id1 : v_id0;
+          let v_id0 = reducedState.getIn(['scene', 'layers', layerID, 'lines', lineID, 'vertices', 0]);
+          let v_id1 = reducedState.getIn(['scene', 'layers', layerID, 'lines', lineID, 'vertices', 1]);
+          let oldVertexID = v_id0 === vertexID ? v_id1 : v_id0;
 
-        let oldVertex = reducedState.getIn(['scene', 'layers', layerID, 'vertices', oldVertexID]);
-        let vertex = reducedState.getIn(['scene', 'layers', layerID, 'vertices', vertexID]);
+          let oldVertex = reducedState.getIn(['scene', 'layers', layerID, 'vertices', oldVertexID]);
+          let vertex = reducedState.getIn(['scene', 'layers', layerID, 'vertices', vertexID]);
 
-        let oldHoles = [];
+          let oldHoles = [];
 
-        let orderedVertices = GeometryUtils.orderVertices([oldVertex, vertex]);
+          let orderedVertices = GeometryUtils.orderVertices([oldVertex, vertex]);
 
-        let holes = reducedState
-          .getIn(['scene', 'layers', layerID, 'lines', lineID, 'holes'])
-          .forEach(holeID => {
-            let hole = reducedState.getIn(['scene', 'layers', layerID, 'holes', holeID]);
-            let oldLineLength = GeometryUtils.pointsDistance(oldVertex.x, oldVertex.y, vertex.x, vertex.y);
-            let offset = GeometryUtils.samePoints( orderedVertices[1], reducedState.getIn(['scene', 'layers', layerID, 'lines', lineID, 'vertices', 1]) ) ? ( 1 - hole.offset ) : hole.offset;
-            let offsetPosition = GeometryUtils.extendLine( oldVertex.x, oldVertex.y, vertex.x, vertex.y, oldLineLength * offset );
+          let holes = reducedState
+            .getIn(['scene', 'layers', layerID, 'lines', lineID, 'holes'])
+            .forEach(holeID => {
+              let hole = reducedState.getIn(['scene', 'layers', layerID, 'holes', holeID]);
+              let oldLineLength = GeometryUtils.pointsDistance(oldVertex.x, oldVertex.y, vertex.x, vertex.y);
+              let offset = GeometryUtils.samePoints( orderedVertices[1], reducedState.getIn(['scene', 'layers', layerID, 'lines', lineID, 'vertices', 1]) ) ? ( 1 - hole.offset ) : hole.offset;
+              let offsetPosition = GeometryUtils.extendLine( oldVertex.x, oldVertex.y, vertex.x, vertex.y, oldLineLength * offset );
 
-            oldHoles.push({hole, offsetPosition});
-          });
-
-        let lineType = reducedState.getIn(['scene', 'layers', layerID, 'lines', lineID, 'type']);
-        let lineProps = reducedState.getIn(['scene', 'layers', layerID, 'lines', lineID, 'properties']);
-        let lineGroups = reducedState   //get groups membership if present
-          .getIn(['scene', 'groups'])
-          .filter( group => group.getIn(['elements', layerID, 'lines']).contains(lineID) );
-        
-        reducedState = Layer.removeZeroLengthLines( reducedState, layerID ).updatedState;
-        reducedState = Layer.mergeEqualsVertices( reducedState, layerID, vertexID ).updatedState;
-        reducedState = Line.remove( reducedState, layerID, lineID ).updatedState;
-
-        if (!GeometryUtils.samePoints(oldVertex, vertex)) {
-          let ret = Line.createAvoidingIntersections(
-            reducedState,
-            layerID,
-            lineType,
-            oldVertex.x,
-            oldVertex.y,
-            vertex.x,
-            vertex.y,
-            lineProps,
-            oldHoles
-          );
-
-          reducedState = ret.updatedState;
-
-          //re-add to old line's groups if present
-          ret.lines.forEach( addedLine => {
-            lineGroups.forEach( oldLineGroup => {
-              reducedState = Group.addElement( reducedState, oldLineGroup.id, layerID, 'lines', addedLine.id ).updatedState;
+              oldHoles.push({hole, offsetPosition});
             });
-          });
-        }
 
-        return reducedState;
-      },
-      state
-    );
+          let lineType = reducedState.getIn(['scene', 'layers', layerID, 'lines', lineID, 'type']);
+          let lineProps = reducedState.getIn(['scene', 'layers', layerID, 'lines', lineID, 'properties']);
+          let lineGroups = reducedState   //get groups membership if present
+            .getIn(['scene', 'groups'])
+            .filter( group => group.getIn(['elements', layerID, 'lines']).contains(lineID) );
+          
+          reducedState = Layer.removeZeroLengthLines( reducedState, layerID ).updatedState;
+          reducedState = Layer.mergeEqualsVertices( reducedState, layerID, vertexID ).updatedState;
+          reducedState = Line.remove( reducedState, layerID, lineID ).updatedState;
 
-    state = Layer.detectAndUpdateAreas( reduced, layerID ).updatedState;
+          if (!GeometryUtils.samePoints(oldVertex, vertex)) {
+            let ret = Line.createAvoidingIntersections(
+              reducedState,
+              layerID,
+              lineType,
+              oldVertex.x,
+              oldVertex.y,
+              vertex.x,
+              vertex.y,
+              lineProps,
+              oldHoles
+            );
+
+            reducedState = ret.updatedState;
+
+            //re-add to old line's groups if present
+            ret.lines.forEach( addedLine => {
+              lineGroups.forEach( oldLineGroup => {
+                reducedState = Group.addElement( reducedState, oldLineGroup.id, layerID, 'lines', addedLine.id ).updatedState;
+              });
+            });
+          }
+
+          return reducedState;
+        },
+        state
+      );
+    }
+
+    state = Layer.detectAndUpdateAreas( state, layerID ).updatedState;
 
     state = state.merge({
       mode: MODE_IDLE,
