69bb879ef2cb3912dfb922557be95de0e9e7c278
Alessio Carrafa <alessio.carrafa@hotmail.it>
2018-06-22 17:36:52 +0200

    moved guides on top of scene but under snap markers

--- src/components/viewer2d/state.jsx
+++ src/components/viewer2d/state.jsx
@@ -23,16 +23,14 @@ export default function State({state, catalog}) {
     <g>
       <rect x="0" y="0" width={width} height={height} fill={SharedStyle.COLORS.white}/>
       <g transform={`translate(0, ${scene.height}) scale(1, -1)`} id="svg-drawing-paper">
+
         <Scene scene={scene} catalog={catalog}/>
+        {scene.getIn(['guides','horizontal']).entrySeq().map( ([ hgKey, hgVal ]) => <line id={'hGuide' + hgKey} key={hgKey} x1={0} y1={hgVal} x2={width} y2={hgVal} style={guideStyle}/> )}
+        {scene.getIn(['guides','vertical']).entrySeq().map( ([ vgKey, vgVal ]) => <line key={vgKey} x1={vgVal} y1={0} x2={vgVal} y2={height} style={guideStyle}/> )}
         {activeSnapElement}
         {snapElements}
+
       </g>
-      {
-        scene.getIn(['guides','horizontal']).entrySeq().map( ([ hgKey, hgVal ]) => <line key={hgKey} x1={0} y1={height - hgVal} x2={width} y2={height - hgVal} style={guideStyle}/> )
-      }
-      {
-        scene.getIn(['guides','vertical']).entrySeq().map( ([ vgKey, vgVal ]) => <line key={vgKey} x1={vgVal} y1={0} x2={vgVal} y2={height} style={guideStyle}/> )
-      }
     </g>
   )
 }
