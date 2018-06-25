3c7377e1ad5d8b5fd4a532f7b8ecd980642d5ee8
Alessio Carrafa <alessio.carrafa@hotmail.it>
2018-06-25 13:39:48 +0200

    improved cube element

--- demo/src/catalog/items/cube/planner-element.jsx
+++ demo/src/catalog/items/cube/planner-element.jsx
@@ -1,59 +1,68 @@
-import {BoxGeometry, MeshBasicMaterial, Mesh, BoxHelper} from 'three';
-
 import React from 'react';
+import { BoxGeometry, MeshBasicMaterial, Mesh, BoxHelper } from 'three';
+import { ReactPlannerSharedStyle } from 'react-planner';
 
 export default {
-  name: "cube",
-  prototype: "items",
+  name: 'cube',
+  prototype: 'items',
 
   info: {
-    title: "cube",
+    title: 'cube',
     tag: ['demo'],
-    description: "Demo item",
+    description: 'Demo item',
     image: require('./cube.png')
   },
 
   properties: {
     color: {
-      label: "Color",
-      type: "color",
-      defaultValue: "#f48342"
+      label: 'Color',
+      type: 'color',
+      defaultValue: ReactPlannerSharedStyle.AREA_MESH_COLOR.unselected
+    },
+    dimension: {
+      label: 'Dimension',
+      type: 'length-measure',
+      defaultValue: {
+        length: 100,
+        unit: 'cm'
+      }
     },
   },
 
   render2D: (element, layer, scene) => {
     let style = {
-      stroke: "#000",
-      strokeWidth: element.selected ? "2px" : "0px",
+      stroke: !element.selected ? ReactPlannerSharedStyle.LINE_MESH_COLOR.unselected : ReactPlannerSharedStyle.MESH_SELECTED,
+      strokeWidth: 2,
       fill: element.properties.get('color')
     };
 
+    let dimension = element.properties.getIn(['dimension', 'length']);
+    let half_dimension = dimension / 2;
+
     return (
-      <g transform="translate(-50, -50)">
-        <rect x="0" y="0" width="100" height="100" style={style}/>
+      <g transform={`translate(-${half_dimension}, -${half_dimension})`}>
+        <rect x="0" y="0" width={dimension} height={dimension} style={style} />
       </g>
     );
   },
 
   render3D: (element, layer, scene) => {
-    let geometry = new BoxGeometry(100, 100, 100);
+    let dimension = element.properties.getIn(['dimension', 'length']);
+    let geometry = new BoxGeometry(dimension, dimension, dimension);
     let material = new MeshBasicMaterial({
       color: element.properties.get('color')
     });
 
     let mesh = new Mesh(geometry, material);
 
-    if (element.selected) {
-      let box = new BoxHelper(mesh, '#000');
-      box.material.linewidth = 1;
-      box.material.depthTest = false;
-      box.renderOrder = 1000;
-      mesh.add(box);
-    }
 
-    mesh.position.y = +50;
+    let box = new BoxHelper(mesh, !element.selected ? ReactPlannerSharedStyle.LINE_MESH_COLOR.unselected : ReactPlannerSharedStyle.MESH_SELECTED );
+    box.material.linewidth = 2;
+    box.renderOrder = 1000;
+    mesh.add(box);
+
+    mesh.position.y = (dimension / 2);
 
     return Promise.resolve(mesh);
   }
-
 };
