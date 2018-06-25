3c3006b8ef56844b968ed421db2f6c267c063856
Alessio Carrafa <alessio.carrafa@hotmail.it>
2018-06-25 12:34:49 +0200

    fixed Wall and Area translation

--- src/catalog/factories/area-factory.jsx
+++ src/catalog/factories/area-factory.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { createArea, updatedArea } from './area-factory-3d';
 import * as SharedStyle from '../../shared-style';
+import Translator from '../../translator/translator';
 
+let translator = new Translator();
 
 export default function AreaFactory(name, info, textures) {
 
@@ -17,12 +19,12 @@ export default function AreaFactory(name, info, textures) {
     },
     properties: {
       patternColor: {
-        label: 'Color',
+        label: translator.t('color'),
         type: 'color',
         defaultValue: SharedStyle.AREA_MESH_COLOR.unselected
       },
       thickness: {
-        label: 'Thickness',
+        label: translator.t('thickness'),
         type: 'length-measure',
         defaultValue: {
           length: 0,
@@ -35,23 +37,23 @@ export default function AreaFactory(name, info, textures) {
       ///print area path
       element.vertices.forEach((vertexID, ind) => {
         let vertex = layer.vertices.get(vertexID);
-        path += ( ind ? 'L' : 'M' ) + vertex.x + ' ' + vertex.y + ' ';
+        path += (ind ? 'L' : 'M') + vertex.x + ' ' + vertex.y + ' ';
       });
 
       //add holes
       element.holes.forEach(areaID => {
-        let area = layer.areas.get( areaID );
+        let area = layer.areas.get(areaID);
 
         area.vertices.reverse().forEach((vertexID, ind) => {
           let vertex = layer.vertices.get(vertexID);
-          path += ( ind ? 'L' : 'M' ) + vertex.x + ' ' + vertex.y + ' ';
+          path += (ind ? 'L' : 'M') + vertex.x + ' ' + vertex.y + ' ';
         });
 
       });
 
       let fill = element.selected ? SharedStyle.AREA_MESH_COLOR.selected : element.properties.get('patternColor');
 
-      return (<path d={path} fill={fill}/>);
+      return (<path d={path} fill={fill} />);
     },
 
     render3D: function (element, layer, scene) {
@@ -66,16 +68,14 @@ export default function AreaFactory(name, info, textures) {
 
   if (textures && textures !== {}) {
 
-    let textureValues = {
-      'none': 'None'
-    };
+    let textureValues = { 'none': 'None' };
 
     for (let textureName in textures) {
       textureValues[textureName] = textures[textureName].name
     }
 
     areaElement.properties.texture = {
-      label: 'Floor',
+      label: translator.t('texture'),
       type: 'enum',
       defaultValue: 'none',
       values: textureValues
--- src/catalog/factories/wall-factory.jsx
+++ src/catalog/factories/wall-factory.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { buildWall, updatedWall } from './wall-factory-3d';
 import * as SharedStyle from '../../shared-style';
 import * as Geometry from '../../utils/geometry';
+import Translator from '../../translator/translator';
 
 const epsilon = 20;
-const STYLE_TEXT = {textAnchor: 'middle'};
-const STYLE_LINE = {stroke: SharedStyle.LINE_MESH_COLOR.selected};
-const STYLE_RECT = {strokeWidth: 1, stroke: SharedStyle.LINE_MESH_COLOR.unselected, fill: 'url(#diagonalFill)'};
+const STYLE_TEXT = { textAnchor: 'middle' };
+const STYLE_LINE = { stroke: SharedStyle.LINE_MESH_COLOR.selected };
+const STYLE_RECT = { strokeWidth: 1, stroke: SharedStyle.LINE_MESH_COLOR.unselected, fill: 'url(#diagonalFill)' };
 const STYLE_RECT_SELECTED = { ...STYLE_RECT, stroke: SharedStyle.LINE_MESH_COLOR.selected };
+
+let translator = new Translator();
 
 export default function WallFactory(name, info, textures) {
 
@@ -17,14 +20,14 @@ export default function WallFactory(name, info, textures) {
     info,
     properties: {
       height: {
-        label: 'Height',
+        label: translator.t('height'),
         type: 'length-measure',
         defaultValue: {
           length: 300,
         }
       },
       thickness: {
-        label: 'Thickness',
+        label: translator.t('thickness'),
         type: 'length-measure',
         defaultValue: {
           length: 20
@@ -33,8 +36,8 @@ export default function WallFactory(name, info, textures) {
     },
 
     render2D: function (element, layer, scene) {
-      let {x:x1, y:y1} = layer.vertices.get(element.vertices.get(0));
-      let {x:x2, y: y2} = layer.vertices.get(element.vertices.get(1));
+      let { x: x1, y: y1 } = layer.vertices.get(element.vertices.get(0));
+      let { x: x2, y: y2 } = layer.vertices.get(element.vertices.get(1));
 
       let length = Geometry.pointsDistance(x1, y1, x2, y2);
       let length_5 = length / 5;
@@ -48,12 +51,12 @@ export default function WallFactory(name, info, textures) {
 
       return (element.selected) ?
         <g>
-          <rect x="0" y={-half_thickness} width={length} height={thickness} style={STYLE_RECT_SELECTED}/>
-          <line x1={length_5} y1={-half_thickness_eps} x2={length_5} y2={half_thickness_eps} style={STYLE_LINE}/>
+          <rect x="0" y={-half_thickness} width={length} height={thickness} style={STYLE_RECT_SELECTED} />
+          <line x1={length_5} y1={-half_thickness_eps} x2={length_5} y2={half_thickness_eps} style={STYLE_LINE} />
           <text x={length_5} y={textDistance + char_height} style={STYLE_TEXT}>A</text>
           <text x={length_5} y={-textDistance} style={STYLE_TEXT}>B</text>
         </g> :
-        <rect x="0" y={-half_thickness} width={length} height={thickness} style={STYLE_RECT}/>
+        <rect x="0" y={-half_thickness} width={length} height={thickness} style={STYLE_RECT} />
     },
 
     render3D: function (element, layer, scene) {
@@ -68,23 +71,21 @@ export default function WallFactory(name, info, textures) {
 
   if (textures && textures !== {}) {
 
-    let textureValues = {
-      'none': 'None'
-    };
+    let textureValues = { 'none': 'None' };
 
     for (let textureName in textures) {
       textureValues[textureName] = textures[textureName].name;
     }
 
     wallElement.properties.textureA = {
-      label: 'Covering A',
+      label: translator.t('texture') + ' A',
       type: 'enum',
       defaultValue: textureValues.bricks ? 'bricks' : 'none',
       values: textureValues
     };
 
     wallElement.properties.textureB = {
-      label: 'Covering B',
+      label: translator.t('texture') + ' B',
       type: 'enum',
       defaultValue: textureValues.bricks ? 'bricks' : 'none',
       values: textureValues
