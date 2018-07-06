e8ed8c0eef001a867e191efc35ea9d50371cc3bb
Alessio Carrafa <alessio.carrafa@hotmail.it>
2018-07-06 17:31:10 +0200

    changed door's texture from png to jpg

--- demo/src/catalog/holes/door/planner-element.jsx
+++ demo/src/catalog/holes/door/planner-element.jsx
@@ -5,49 +5,55 @@ import path from 'path';
 
 let cached3DDoor = null;
 
+const STYLE_HOLE_BASE = {stroke: '#000', strokeWidth: '3px', fill: '#000'};
+const STYLE_HOLE_SELECTED = {stroke: '#0096fd', strokeWidth: '4px', fill: '#0096fd', cursor: 'move'};
+const STYLE_ARC_BASE = {stroke: '#000', strokeWidth: '3px', strokeDasharray: '5,5', fill: 'none'};
+const STYLE_ARC_SELECTED = {stroke: '#0096fd', strokeWidth: '4px', strokeDasharray: '5,5', fill: 'none', cursor: 'move'};
+const EPSILON = 3;
+
 export default {
-  name: "door",
-  prototype: "holes",
+  name: 'door',
+  prototype: 'holes',
 
   info: {
-    title: "door",
+    title: 'door',
     tag: ['door'],
-    description: "Wooden door",
+    description: 'Wooden door',
     image: require('./door.png')
   },
 
   properties: {
     width: {
-      label: "Width",
-      type: "length-measure",
+      label: 'Width',
+      type: 'length-measure',
       defaultValue: {
         length: 80
       }
     },
     height: {
-      label: "Height",
-      type: "length-measure",
+      label: 'Height',
+      type: 'length-measure',
       defaultValue: {
         length: 215
       }
     },
     altitude: {
-      label: "Altitude",
-      type: "length-measure",
+      label: 'Altitude',
+      type: 'length-measure',
       defaultValue: {
         length: 0
       }
     },
     thickness: {
-      label: "Thickness",
-      type: "length-measure",
+      label: 'Thickness',
+      type: 'length-measure',
       defaultValue: {
         length: 30
       }
     },
     flip_orizzontal: {
-      label: "flip orizzontale",
-      type: "checkbox",
+      label: 'flip orizzontale',
+      type: 'checkbox',
       defaultValue: false,
       values: {
         'none': false,
@@ -57,16 +63,9 @@ export default {
   },
 
   render2D: function (element, layer, scene) {
-    const STYLE_HOLE_BASE = {stroke: "#000", strokeWidth: "3px", fill: "#000"};
-    const STYLE_HOLE_SELECTED = {stroke: "#0096fd", strokeWidth: "4px", fill: "#0096fd", cursor: "move"};
-    const STYLE_ARC_BASE = {stroke: "#000", strokeWidth: "3px", strokeDasharray: "5,5", fill: "none"};
-    const STYLE_ARC_SELECTED = {stroke: "#0096fd", strokeWidth: "4px", strokeDasharray: "5,5", fill: "none", cursor: "move"};
-
-    let epsilon = 3;
-
     let flip = element.properties.get('flip_orizzontal');
     let holeWidth = element.properties.get('width').get('length');
-    let holePath = `M${0} ${ -epsilon}  L${holeWidth} ${-epsilon}  L${holeWidth} ${epsilon}  L${0} ${epsilon}  z`;
+    let holePath = `M${0} ${ -EPSILON}  L${holeWidth} ${-EPSILON}  L${holeWidth} ${EPSILON}  L${0} ${EPSILON}  z`;
     let arcPath = `M${0},${0}  A${holeWidth},${holeWidth} 0 0,1 ${holeWidth},${holeWidth}`;
     let holeStyle = element.selected ? STYLE_HOLE_SELECTED : STYLE_HOLE_BASE;
     let arcStyle = element.selected ? STYLE_ARC_SELECTED : STYLE_ARC_BASE;
@@ -75,22 +74,18 @@ export default {
     if(flip == false) {
       return (
         <g transform={`translate(${-length / 2}, 0)`}>
-          <path key="1" d={arcPath} style={arcStyle}
-                transform={`translate(${0},${holeWidth}) scale(${1},${-1}) rotate(${0})`}/>
-          <line key="2" x1={0} y1={holeWidth - epsilon} x2={0} y2={0 - epsilon} style={holeStyle}
-                transform={`scale(${-1},${1})`}/>
-          <path key="5" d={holePath} style={holeStyle}/>
+          <path d={arcPath} style={arcStyle} transform={`translate(${0},${holeWidth}) scale(${1},${-1}) rotate(${0})`}/>
+          <line x1={0} y1={holeWidth - EPSILON} x2={0} y2={0 - EPSILON} style={holeStyle} transform={`scale(${-1},${1})`}/>
+          <path d={holePath} style={holeStyle}/>
         </g>
       )
     }
     else{
       return (
         <g transform={`translate(${-length / 2}, 0)`}>
-          <path key="1" d={arcPath} style={arcStyle}
-                transform={`translate(${0},${-holeWidth}) scale(${1},${1}) rotate(${0})`}/>
-          <line key="2" x1={0} y1={-holeWidth - epsilon} x2={0} y2={0 - epsilon} style={holeStyle}
-                transform={`scale(${-1},${1})`}/>
-          <path key="5" d={holePath} style={holeStyle}/>
+          <path d={arcPath} style={arcStyle} transform={`translate(${0},${-holeWidth}) scale(${1},${1}) rotate(${0})`}/>
+          <line x1={0} y1={-holeWidth - EPSILON} x2={0} y2={0 - EPSILON} style={holeStyle} transform={`scale(${-1},${1})`}/>
+          <path d={holePath} style={holeStyle}/>
         </g>
       )
     }
@@ -128,7 +123,7 @@ export default {
 
     let mtl = require('./door.mtl');
     let obj = require('./door.obj');
-    let img = require('./texture.png');
+    let img = require('./texture.jpg');
 
     return loadObjWithMaterial(mtl, obj, path.dirname(img) + '/')
       .then(object => {
