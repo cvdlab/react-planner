8d748c79c8b2f7b105d0d2fc1d00241fc54c4060
ParameswaranSajeenthiran <psajeendran@gmail.com>
2023-01-13 16:22:07 +0530

    blinking action

--- es/plugins/autosave.js
+++ es/plugins/autosave.js
@@ -15,17 +15,36 @@ export default function autosave(autosaveKey, delay,requestURL,layoutId) {
     if (!localStorage) return;
 
     //revert
-    // if (localStorage.getItem(autosaveKey) !== null) {
-    //   var data = localStorage.getItem(autosaveKey);
-    //   var json = JSON.parse(data);
-    //   store.dispatch(loadProject(json));
-    // }
-    var path=requestURL //have to modify
-    axios.get(path).then((res)=>{
-       data =res.data
-       store.dispatch(loadProject(json));   
+    if (localStorage.getItem(autosaveKey) !== null) {
+      var data = localStorage.getItem(autosaveKey);
+      var json = JSON.parse(data);
+
+
+      var items=json["layers"]["layer-1"]["items"]
+      console.log(json["layers"]["layer-1"]["items"],json)
+      var count=0
+      
+      setInterval(function() {
+        if (count%2==0){
+          console.log("test",count)
+          json["layers"]["layer-1"]["items"]={}
+        }else{
+          console.log("test",count)
+          json["layers"]["layer-1"]["items"]=items
+        }
+        count=count+1
+        store.dispatch(loadProject(json));
+
+        console.log("Interval reached every 5s")
+      },1000);
+      store.dispatch(loadProject(json));
+    }
+    // var path=requestURL //have to modify
+    // axios.get(path).then((res)=>{
+    //    data =res.data
+    //    store.dispatch(loadProject(json));   
     
-    })
+    // })
 
     //update
     store.subscribe(function () {
