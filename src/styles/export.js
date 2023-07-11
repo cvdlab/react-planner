d78d966fcaed00b4fc23a104042a195ecf9b5bed
Pascal Gula <pascal.gula@gmail.com>
2023-07-11 11:59:28 +0200

    move CSS to CSS in JS for Tab Styles to enable import in next.js app

--- src/styles/export.js
+++ src/styles/export.js
@@ -1,4 +1,33 @@
-import * as TabsStyle from './tabs.css';
+// import * as TabsStyle from './tabs.css';
+
+const TabsStyle = {
+  tabList: {
+    borderBottom: '1px solid #aaa',
+    margin: '0 0 10px',
+    padding: '0',
+  },
+  tab: {
+    display: 'inline-block',
+    border: '1px solid transparent',
+    borderBottom: 'none',
+    bottom: '-1px',
+    position: 'relative',
+    listStyle: 'none',
+    padding: '6px 12px',
+    cursor: 'pointer',
+  },
+  selectedTab: {
+    borderColor: '#aaa',
+    color: '#1CA6FC',
+    outline: 'none',
+  },
+  tabPanel: {
+    display: 'none',
+  },
+  selectedTabPanel: {
+    display: 'block',
+  },
+};
 
 export {
   TabsStyle
